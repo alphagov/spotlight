28069203e82a4fca58ba537650de309621bdd657
Paulo Schneider <paulo.schneider@gmail.com>
2013-10-16 11:21:16 +0100

    Start and stop server before and after running scenario

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,3 +5,13 @@ require 'rspec/expectations'
 Capybara.default_driver = :poltergeist
 Capybara.app_host = 'http://localhost:3000'
 Capybara.run_server = false
+
+Before do 
+  $server = fork { exec 'node app/server.js' } 
+  sleep 1
+end 
+
+After do
+  Process.kill("INT", $server)
+  Process.wait($server)
+end
