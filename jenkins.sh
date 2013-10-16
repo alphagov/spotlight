4aeeefe31fd631801038e8366d50acf6260f8561
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-10-16 17:01:25 +0100

    Use nodeenv on Jenkins
    We don't have any git submodules anymore.

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
+NODEENV=/var/tmp/nodeenvs/$(echo ${JOB_NAME} | tr ' ' '-')
+
+if [ -d "$NODEENV" ]; then
+    source $NODEENV/bin/activate
+else
+    nodeenv --node=0.10.20 $NODEENV
+    source $NODEENV/bin/activate
+    npm install -g grunt-cli@0.1.9
+fi
+
 npm install
-git submodule init
-git submodule update
 
 npm test
