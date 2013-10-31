bd93cb9e5e14b5c88318f56751b5c14153a96f49
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-10-31 16:51:52 +0000

    Make backdrop-stub return 404 if no data available

--- app/support/backdrop_stub/backdrop_stub_controller.js
+++ app/support/backdrop_stub/backdrop_stub_controller.js
@@ -1,13 +1,14 @@
-define(['support/backdrop_stub/response_fetcher'], function (ResponseFetcher) {
-
-  return function(req, res){
+define([
+  'support/backdrop_stub/response_fetcher'
+], function (ResponseFetcher) {
+  return function (req, res) {
     var fetcher = new ResponseFetcher();
     var json = fetcher.fetch_json(req);
     if (json) {
       return res.json(json);
     } else {
+      res.status(404);
       return res.json({error: "no matching response found"});
     }
   };
-
 });
