007a9232c896ebe3f52df06aa36eab2ddc34dc06
Jacob Ashdown <jcbashdown@googlemail.com>
2013-11-01 17:09:36 +0000

    trying to get features working
    no section classes set for modules, can;t trace why yet

---
+++ features/step_definitions/module_steps.rb
@@ -0,0 +1,25 @@
+def values
+  {
+    'realtime' => {
+      'no-realistic-dashboard' => {
+        title: 'Real-time usage',
+        description: 'Real-time usage',
+        raw: 15
+      }
+    }
+  }
+end
+
+Then(/^I should see the "(.*?)" module for "(.*?)" data$/) do |display_module, service|
+  page.find(".#{display_module} .visualisation").should have_content(values[display_module][service][:raw])
+end
+
+Then(/^I should not see other information for the "(.*?)" "(.*?)" module$/) do |service, display_module|
+  page.find(".#{display_module} .visualisation").should_not have_content(values[display_module][service][:title])
+  page.find(".#{display_module} .visualisation").should_not have_content(values[display_module][service][:description])
+end
+
+Then(/^I should see other information for the "(.*?)" "(.*?)" module$/) do |service, display_module|
+  page.find(".#{display_module} .visualisation").should have_content(values[display_module][service][:title])
+  page.find(".#{display_module} .visualisation").should have_content(values[display_module][service][:description])
+end 
