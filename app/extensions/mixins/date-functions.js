798c958a7dfd1458b3d1835be2f9e8b4dc9b28ef
Oliver Spindler <os@oliverspindler.com>
2013-11-26 10:06:36 +0000

    Minor refactor

--- app/extensions/mixins/date-functions.js
+++ app/extensions/mixins/date-functions.js
@@ -1,38 +1,34 @@
-define(  
-  function () {
-    return (function () {
-      return {
-        latest: function (objects, filter) {
-          var dates = _.map(objects, filter);
-          if(dates.length < 1){
-            return dates.first;
-          }
-          return _.reduce(dates, function (latest, current) {
-            return current.isAfter(latest) ? current : latest;
-          });
-        },
+define(function () {
+  return {
+    latest: function (objects, filter) {
+      var dates = _.map(objects, filter);
+      if(dates.length < 1){
+        return dates.first;
+      }
+      return _.reduce(dates, function (latest, current) {
+        return current.isAfter(latest) ? current : latest;
+      });
+    },
 
-        earliest: function (objects, filter) {
-          var dates = _.map(objects, filter);
-          if(dates.length < 1){
-            return dates.first;
-          }
-          return _.reduce(dates, function (earliest, current) {
-            return current.isBefore(earliest) ? current : earliest;
-          });
-        },
+    earliest: function (objects, filter) {
+      var dates = _.map(objects, filter);
+      if(dates.length < 1){
+        return dates.first;
+      }
+      return _.reduce(dates, function (earliest, current) {
+        return current.isBefore(earliest) ? current : earliest;
+      });
+    },
 
-        numberOfWeeksInPeriod: function (start, end) {
-          return end.diff(start, "week");
-        },
+    numberOfWeeksInPeriod: function (start, end) {
+      return end.diff(start, "week");
+    },
 
-        weeksFrom: function (latestDate, numberOfWeeksToGenerate) {
-          return _.times(numberOfWeeksToGenerate, function (i) {
-            var weeksAgo = numberOfWeeksToGenerate - i - 1;
-            return latestDate.clone().subtract(weeksAgo, "weeks");
-          });
-        }
-      };
-    }());
-  }
-);
+    weeksFrom: function (latestDate, numberOfWeeksToGenerate) {
+      return _.times(numberOfWeeksToGenerate, function (i) {
+        var weeksAgo = numberOfWeeksToGenerate - i - 1;
+        return latestDate.clone().subtract(weeksAgo, "weeks");
+      });
+    }
+  };
+});
