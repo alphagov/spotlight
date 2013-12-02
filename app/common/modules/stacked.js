5b4ab68497e392a151488710b625c192b9cce4a1
Jacob Ashdown <jcbashdown@gmail.com>
2013-12-02 17:15:13 +0000

    stub json and mapping, query in module (module name needs thought)

---
+++ app/common/modules/stacked.js
@@ -0,0 +1,3 @@
+//https://www.preview.performance.service.gov.uk/data/housing-policy/residential-transactions?collect=value%3Asum&period=month&group_by=geography&filter_by=key%3Aresidential_property_transactions&end_at=2013-12-01T00%3A00%3A00%2B00%3A00&start_at=2012-12-01T00%3A00%3A00%2B00%3A00
+
+//https://www.preview.performance.service.gov.uk/data/housing-policy/residential-transactions
