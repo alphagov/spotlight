b79bc49608e9e0e8ce41c8437b586a91493c871a
Jacob Ashdown <jcbashdown@gmail.com>
2013-12-06 16:40:22 +0000

    Working policies on dashboard
    may want extracting into subview
    needs styling

--- app/common/views/dashboard.js
+++ app/common/views/dashboard.js
@@ -6,8 +6,19 @@ function (GovUkView, contentTemplate) {
   var DashboardView = GovUkView.extend({
     contentTemplate: contentTemplate,
 
+    extendRelatedPagesContext: function (context) {
+      _.each(context.related_pages, function (page, i){
+        context.related_pages[i].formatted_public_timestamp = this.getFormattedTimestamp(page.public_timestamp);
+      }, this);
+      return context;
+    },
+
+    getFormattedTimestamp: function (timestamp) {
+      return this.getMoment(timestamp).format("Do MMMM YYYY");
+    },
+
     getContent: function () {
-      var context = this.model.toJSON();
+      context = this.extendRelatedPagesContext(this.model.toJSON());
 
       context.modules = _.map(this.moduleInstances, function (module) {
         return module.html;
