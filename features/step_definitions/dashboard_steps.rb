e8cef5f5cfe83837dddc280d20e2a5345cc76daa
Jacob Ashdown <jcbashdown@gmail.com>
2013-12-09 14:35:58 +0000

    Add cucumber test for related pages

--- features/step_definitions/dashboard_steps.rb
+++ features/step_definitions/dashboard_steps.rb
@@ -13,3 +13,22 @@ end
 Then(/^I see the tagline "(.*?)"$/) do |expected_tagline|
   page.find("#content header .tagline").should have_content(expected_tagline)
 end
+
+Then(/^I should see the "(.*?)" information for "(.*?)"$/) do |section, service|
+  section_values[section][service].each do |text|
+    page.find(".#{section}").should have_content(text)
+  end
+end
+
+def section_values
+  {
+    "related-pages" => { 
+      "no-realistic-dashboard" => [
+        "Helping people to buy a home",
+        "Increasing the number of available homes",
+        "Improving the rented housing sector",
+        "Providing housing support for older and vulnerable people"
+      ]
+    }
+  }
+end
