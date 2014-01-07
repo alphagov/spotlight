75d52c0bcb318bfb8b597951b2cf18a337f0af55
Anna Powell-Smith <anna.powell-smith@digital.cabinet-office.gov.uk>
2014-01-07 16:41:13 +0000

    Update with latest Backdrop data; update Stagecraft stub; amend line labels

--- app/support/backdrop_stub/response_fetcher.js
+++ app/support/backdrop_stub/response_fetcher.js
@@ -41,8 +41,8 @@ define(['extensions/models/model', 'underscore', 'fs', 'path'], function (Model,
       { 'key': {'service': 'housing-policy', 'api_name': 'first-time-buyer'}, 'file': 'housing-first-time-buyer.json'},
       { 'key': {'service': 'carers-allowance', 'api_name': 'weekly-claims', 'collect': 'value:sum', 'period': 'week', 'group_by': 'key'}, 'file': 'carers_allowance_weekly_by_channel.json'},
       { 'key': {'service': 'carers-allowance', 'api_name': 'weekly-claims'}, 'file': 'carers-allowance-weekly-claims.json'},
-      { 'key': {'service': 'gcloud', 'api_name': 'sales', "period": "month", "collect": "monthly_spend:sum", "group_by": "lot"}, 'file': 'g-cloud-monthly-by-lot.json'},
-      { 'key': {'service': 'gcloud', 'api_name': 'sales', "period": "month", "collect": "monthly_spend:sum", "group_by": "sector"}, 'file': 'g-cloud-who-is-using.json'}
+      { 'key': {'service': 'gcloud', 'api_name': 'sales', "period": "month", "collect": "cumulative_spend:sum", "group_by": "sector"}, 'file': 'g-cloud-who-is-using.json'},
+      { 'key': {'service': 'gcloud', 'api_name': 'sales', "period": "month", "collect": "monthly_spend:sum", "group_by": "lot"}, 'file': 'g-cloud-monthly-by-lot.json'}
     ],
 
     fetch_json: function (request) {
