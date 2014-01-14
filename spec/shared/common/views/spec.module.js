1caddf18b41a78ce606287802d76dd7cf50fd2be
Anna Powell-Smith <anna.powell-smith@digital.cabinet-office.gov.uk>
2014-01-14 14:08:48 +0000

    Reduce heading levels within module sections, for accessibility

--- spec/shared/common/views/spec.module.js
+++ spec/shared/common/views/spec.module.js
@@ -33,27 +33,27 @@ function (ModuleView, Collection, Model, View) {
 
     it("renders a module", function () {
       moduleView.render();
-      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h1 id="A-Title-heading">A Title</h1><div class="visualisation">test content</div></section>');
+      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h2 id="A-Title-heading">A Title</h2><div class="visualisation">test content</div></section>');
     });
 
     it("renders a module with description", function () {
       model.set('description', 'Description');
       moduleView.render();
-      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h1 id="A-Title-heading">A Title</h1><h2>Description</h2><div class="visualisation">test content</div></section>');
+      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h2 id="A-Title-heading">A Title</h2><h3>Description</h3><div class="visualisation">test content</div></section>');
     });
 
     it("renders a module with description and info", function () {
       model.set('description', 'Description');
       model.set('info', ['Info line 1', 'Info line 2']);
       moduleView.render();
-      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><details class="more-info" role="complementary"><summary class="more-info-link">more info</summary><ul><li>Info line 1</li><li>Info line 2</li></ul></details><h1 id="A-Title-heading">A Title</h1><h2>Description</h2><div class="visualisation">test content</div></section>');
+      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><details class="more-info" role="complementary"><summary class="more-info-link">more info</summary><ul><li>Info line 1</li><li>Info line 2</li></ul></details><h2 id="A-Title-heading">A Title</h2><h3>Description</h3><div class="visualisation">test content</div></section>');
     });
 
     it("renders a module with description and info link", function () {
       model.set('description', 'Description');
       model.set('info', ['<a href="https://example.com/">Info line 1</a> with trailing text', 'Info line 2']);
       moduleView.render();
-      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><details class="more-info" role="complementary"><summary class="more-info-link">more info</summary><ul><li><a href="https://example.com/">Info line 1</a> with trailing text</li><li>Info line 2</li></ul></details><h1 id="A-Title-heading">A Title</h1><h2>Description</h2><div class="visualisation">test content</div></section>');
+      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><details class="more-info" role="complementary"><summary class="more-info-link">more info</summary><ul><li><a href="https://example.com/">Info line 1</a> with trailing text</li><li>Info line 2</li></ul></details><h2 id="A-Title-heading">A Title</h2><h3>Description</h3><div class="visualisation">test content</div></section>');
     });
 
     it("renders an SVG-based module as a fallback element on the server", function () {
@@ -68,7 +68,7 @@ function (ModuleView, Collection, Model, View) {
           .replace('&gt;', '>')
           .replace(' />', '/>');
 
-        expect(content).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h1 id="A-Title-heading">A Title</h1><div class="visualisation-fallback" data-src="/test/url.png"><noscript><img src="/test/url.png"/></noscript></div></section>');
+        expect(content).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h2 id="A-Title-heading">A Title</h2><div class="visualisation-fallback" data-src="/test/url.png"><noscript><img src="/test/url.png"/></noscript></div></section>');
       });
     });
 
@@ -76,20 +76,20 @@ function (ModuleView, Collection, Model, View) {
       jasmine.clientOnly(function () {
         moduleView.requiresSvg = true;
         moduleView.render();
-        expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h1 id="A-Title-heading">A Title</h1><div class="visualisation">test content</div></section>');
+        expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h2 id="A-Title-heading">A Title</h2><div class="visualisation">test content</div></section>');
       });
     });
 
     it("renders a module with classes module-banner and restricted-data-banner when Stagecraft sets restricted_data to true", function () {
       model.set('restricted_data', true);
       moduleView.render();
-      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass module-banner restricted-data-banner"><div class="module-banner-text"><p>This section contains <strong>commercially licensed data</strong>. Do not share or redistribute outside government.</p></div><h1 id="A-Title-heading">A Title</h1><div class="visualisation">test content</div></section>');
+      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass module-banner restricted-data-banner"><div class="module-banner-text"><p>This section contains <strong>commercially licensed data</strong>. Do not share or redistribute outside government.</p></div><h2 id="A-Title-heading">A Title</h2><div class="visualisation">test content</div></section>');
     });
 
     it("renders a module without classes module-banner and restricted-data-banner when Stagecraft returns restricted_data false", function () {
       model.set('restricted_data', false);
       moduleView.render();
-      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h1 id="A-Title-heading">A Title</h1><div class="visualisation">test content</div></section>');
+      expect(getContent()).toEqual('<section aria-labelledby="A-Title-heading" role="region" class="testclass"><h2 id="A-Title-heading">A Title</h2><div class="visualisation">test content</div></section>');
     });
   });
 });
