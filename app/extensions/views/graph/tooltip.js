1f240f7cabd718d1a6be12083ff90d4b314a0ffe
Jacob Ashdown <jcbashdown@gmail.com>
2014-01-14 14:50:29 +0000

    remove comments, add explanations of hacks, use constant for LABELS_OFF

--- app/extensions/views/graph/tooltip.js
+++ app/extensions/views/graph/tooltip.js
@@ -3,8 +3,9 @@ define([
   'extensions/mixins/pivot'
 ],
 function (Component, Pivot) {
-  var Tooltip = Component.extend({
+  var LABELS_OFF = "no labels";
 
+  var Tooltip = Component.extend({
     classed: 'tooltip',
     constrainToBounds: true,
     horizontal: 'right',
@@ -32,10 +33,22 @@ function (Component, Pivot) {
         var sum = _.reduce(model, function(sum, model){
           return sum += model.get(this.graph.valueAttr);
         }, 0, this);
+        //currently summing only happens in a module - 
+        //grouped time series displayed as a stack - 
+        //where null signifies no data at that point and these reduce to 0.
+        //As a result we have this hack to force a no data label
+        //it would be better to acknowledge the nulls rather than sum to zeros
+        //as we would be able to better deal with 0s
         if(sum === 0){
           sum = null;
+          //this is a hack based on a bug in getDistanceAndClosestModel
+          //which manifests in grouped_timeseries displayed at stack
+          //which causes the total rather than the stack value to be displayed
+          //when hovering to the right of the last value.
+          //As this value is not 0 when summed we turn off labels when there is a sum
+          //and noTotal is true 
         }else if(this.noTotal){
-          sum = "no total";
+          sum = LABELS_OFF;
         }
         return sum;
       }else{
@@ -52,7 +65,6 @@ function (Component, Pivot) {
     },
 
     onChangeSelected: function (group, groupIndex, model, index) {
-      //make handle groups being array
       var unselected = model == null;
       var selection = this.componentWrapper.selectAll('text');
 
@@ -63,7 +75,7 @@ function (Component, Pivot) {
 
       var value = this.getValue(group, groupIndex, model, index);
 
-      if(value == "no total"){
+      if(value == LABELS_OFF){
         selection.data([]).exit().remove();
         return;
       }
