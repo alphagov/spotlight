a18ae6fd8f384622a279e1570c0ea33a7693a802
Anna Powell-Smith <anna.powell-smith@digital.cabinet-office.gov.uk>
2014-01-28 13:30:10 +0000

    Initial commit of sparkline for realtime graphs.
    Changes:
    
    * Add configurable time period option to module/collection.
    * Convert realtime collection to matrix collection.
    * Update view to render sparkline, add hover listeners.
    * Remove interpolation methods from view, since these don't work well with
      sparklines (discussed this change with Phil).
    * Add and update relevant styles.
    * Update tests, including Cucumber tests.

--- app/common/views/visualisations/multi_stat_item/sparkline.js
+++ app/common/views/visualisations/multi_stat_item/sparkline.js
@@ -6,18 +6,37 @@ function (Graph) {
     
     minYDomainExtent: 0, 
     
+    initialize: function (options) {
+      Graph.prototype.initialize.apply(this, arguments);
+
+      this.period = options.period;
+      this.showTooltip = options.showTooltip;
+    },
+
     getConfigNames: function () {
-      return ['overlay', 'month', 'ymin']; 
+      return ['overlay', this.period || 'month', 'ymin'];
     },
     
     components: function () {
-      return [
+      var val = [
         {
           view: this.sharedComponents.line,
           options: { allowMissingData: true }
         },
         { view: this.sharedComponents.hover }
       ];
+      if (this.showTooltip) {
+        var tooltipComponent = {
+          view: this.sharedComponents.tooltip,
+          options: {
+            formatValue: function (value) {
+              return this.formatNumericLabel(value);
+            }
+          }
+        };
+        val.push(tooltipComponent);
+      }
+      return val;
     }
   });
 
--- spec/shared/common/views/visualisations/spec.visitors-realtime.js
+++ spec/shared/common/views/visualisations/spec.visitors-realtime.js
@@ -4,75 +4,207 @@ define([
 ],
 function (VisitorsRealtimeView, Collection) {
   describe("VisitorsRealtimeView", function () {
-    it("renders a number", function () {
-      var view = new VisitorsRealtimeView({
-        collection: new Collection([{
-          "unique_visitors": 10
-        }])
-      });
 
-      jasmine.renderView(view, function () {
-        expect(view.$el.html()).toEqual(
-          '<p>  <span class="impact-number"><strong>10</strong></span>  <span class="stat-description">users online now</span></p>'
-        );
+    var collection, view, model;
+    beforeEach(function() {
+      collection = new Collection();
+      var data = [
+        {
+          _timestamp: collection.getMoment("2002-03-01T00:00:00+00:00"),
+          unique_visitors: 120
+        },
+        {
+          _timestamp: collection.getMoment("2002-03-01T00:03:00+00:00"),
+          unique_visitors: '100'
+        }
+      ];
+      collection.reset([ {
+        id: 'test',
+        title: 'test',
+        values: new Collection(data)
+      } ]);
+
+      view = new VisitorsRealtimeView({
+        collection: collection
+      });
+      jasmine.serverOnly(function () {
+        view.sparkline = false;
       });
     });
 
-    it("renders singular if one user", function () {
-      var view = new VisitorsRealtimeView({
-        collection: new Collection([{
-          "unique_visitors": 1
-        }])
-      });
-
+    it("renders number of users at most recent timestamp", function () {
       jasmine.renderView(view, function () {
-        expect(view.$el.find('.stat-description').text()).toEqual('user online now');
+        expect(view.$el.find('.impact-number strong').text()).toEqual('100');
       });
     });
 
-    describe('interpolating numbers', function () {
-      beforeEach(function() {
-        jasmine.Clock.useMock();
+    it("renders correct labels", function () {
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('.stat-description').text()).toEqual("users online at 12am 1 March 2002");
+        expect(view.$el.find('.sparkline-title').text()).toEqual("users in the 3 minutes to 12am 1 March 2002");
+      });
+    });
+
+    it("renders correct labels for data near now", function () {
+
+      var testView, testCollection;
+      testCollection = new Collection();
+
+      var testData = [
+        {
+          _timestamp: testCollection.moment().subtract('minutes', 3),
+          unique_visitors: 3
+        },
+        {
+          _timestamp: testCollection.moment(),
+          unique_visitors: 5
+        }
+      ];
+      testCollection.reset([ {
+        id: 'test',
+        title: 'test',
+        values: new Collection(testData)
+      } ]);
+      testView = new VisitorsRealtimeView({
+        collection: testCollection
+      });
+      jasmine.serverOnly(function () {
+        testView.sparkline = false;
       });
 
-      it("interpolates from previous to latest entry", function () {
-        jasmine.clientOnly(function () {
-          var collection = new Collection([
-            { "unique_visitors": 10 }
-          ]);
-          collection.updateInterval = 12000;
-          var view = new VisitorsRealtimeView({
-            collection: collection
-          });
-          jasmine.renderView(view, function () {
-            collection.set([{ "unique_visitors": 34 }]);
-            collection.trigger('sync');
-            expect(view.$el.find('strong')).toHaveHtml('11');
-            jasmine.Clock.tick(view.updateInterval);
-            // first tick
-            expect(view.$el.find('strong')).toHaveHtml('12');
-            jasmine.Clock.tick(view.updateInterval);
-            // second tick
-            expect(view.$el.find('strong')).toHaveHtml('13');
-            jasmine.Clock.tick(view.updateInterval * 21);
-            // reached the new value
-            expect(view.$el.find('strong')).toHaveHtml('34');
-            jasmine.Clock.tick(view.updateInterval * 10);
-            // nothing happens, new value was already reached
-            expect(view.$el.find('strong')).toHaveHtml('34');
-          });
-        });
+      jasmine.renderView(testView, function () {
+        expect(testView.$el.find('.stat-description').text()).toEqual("users online now");
+        expect(testView.$el.find('.sparkline-title').text()).toEqual("Users over past 3 minutes");
       });
+    });
+
+    it("renders singular labels if there is just one user", function () {
+
+      var testView, testCollection;
+      testCollection = new Collection();
+      var testData = [
+        {
+          _timestamp: collection.getMoment("2002-03-01T00:00:00+00:00"),
+          unique_visitors: 3
+        },
+        {
+          _timestamp: collection.getMoment("2002-03-01T00:03:00+00:00"),
+          unique_visitors: 1
+        }
+      ];
+      testCollection.reset([ {
+        id: 'test',
+        title: 'test',
+        values: new Collection(testData)
+      } ]);
+      testView = new VisitorsRealtimeView({
+        collection: testCollection
+      });
+      jasmine.serverOnly(function () {
+        testView.sparkline = false;
+      });
+
+      jasmine.renderView(testView, function () {
+        expect(testView.$el.find('.stat-description').text()).toEqual('user online at 12am 1 March 2002');
+      });
+    });
+
+    it("does not render a sparkline if there is only one data item", function () {
+
+      var testView, testCollection;
+      testCollection = new Collection();
+      var testData = [
+        {
+          _timestamp: collection.getMoment("2002-03-01T00:00:00+00:00"),
+          unique_visitors: 3
+        }
+      ];
+      testCollection.reset([ {
+        id: 'test',
+        title: 'test',
+        values: new Collection(testData)
+      } ]);
+      testView = new VisitorsRealtimeView({
+        collection: testCollection
+      });
+      jasmine.serverOnly(function () {
+        testView.sparkline = false;
+      });
+
+      jasmine.renderView(testView, function () {
+        expect(testView.$el.find('figure').length).toEqual(0);
+      });
+
     });
 
     it("renders nothing when there's no data", function () {
-      var view = new VisitorsRealtimeView({
-        collection: new Collection([])
+      var testView, testCollection;
+      testCollection = new Collection();
+      testCollection.reset([ {
+        id: 'test',
+        title: 'test',
+        values: new Collection([])
+      } ]);
+      testView = new VisitorsRealtimeView({
+        collection: testCollection
+      });
+      jasmine.serverOnly(function () {
+        testView.sparkline = false;
       });
 
-      jasmine.renderView(view, function () {
-        expect(view.$el.html()).toEqual('');
+      jasmine.renderView(testView, function () {
+        expect(testView.$el.find('div').length).toEqual(0);
       });
     });
+
+    describe("getValue", function () {
+      it("should return the right value", function (){
+        var return_value = view.getValue();
+        expect(return_value).toEqual("100");
+      });
+    });
+
+    describe("getLabel", function () {
+      it("should return the right value", function (){
+        var return_value = view.getLabel();
+        expect(return_value.headline).toEqual("users online at<br/> 12am 1 March 2002");
+        expect(return_value.graph).toEqual("users in the 3 minutes to<br/> 12am 1 March 2002");
+      });
+    });
+
+    describe("getValueSelected", function () {
+      it("should return the right value", function (){
+        var fakeSelection = {
+          selectedModel: {
+            get: function (key){
+              return {
+                'unique_visitors': 140
+              }[key];
+            }
+          }
+        };
+        var return_value = view.getValueSelected(fakeSelection);
+        expect(return_value).toEqual("140");
+      });
+    });
+
+    describe("getLabelSelected", function () {
+      it("should call the view formatPeriod method with the appropriate selectedModel attribute", function (){
+        var fakeSelection = {
+          selectedModel: {
+            get: function (key){
+              return {
+                '_timestamp': collection.getMoment("2002-03-01T00:06:00+00:00"),
+                'unique_visitors': 140
+              }[key];
+            }
+          }
+        };
+        var return_value = view.getLabelSelected(fakeSelection);
+        expect(return_value.headline).toEqual('users 03/01/2002, 12 years ago');
+        expect(return_value.graph).toEqual('12 years ago');
+      });
+    });
+
   });
 });
