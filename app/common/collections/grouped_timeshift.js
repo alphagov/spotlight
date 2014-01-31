a36f359f9fd4661978a6361f67d722d273492eb3
Edd Sowden <e@e26.co.uk>
2014-01-31 13:38:33 +0000

    Add GroupedTimeshiftCollection to timeshift data
    For some graphs we want to be able to show a series timeshifted from
    the other series. Add a collection which will increse the duration
    required to cover all the series. Then slice the return values so the
    correct parts of data are selected for each series.
    
    Allow the grouped_timeseries module to automatically switch to this
    collection if it detects configuration asking for a timeshifted series.

---
+++ app/common/collections/grouped_timeshift.js
@@ -0,0 +1,81 @@
+define([
+  'extensions/collections/matrix',
+  'extensions/models/query'
+],
+function (MatrixCollection, Query) {
+
+  var GroupedTimeshiftCollection = MatrixCollection.extend({
+    queryParams: function () {
+      return {
+        collect: this.options.valueAttr,
+        period: this.options.period,
+        group_by: this.options.category,
+        filter_by: this.options.filterBy ? this.options.filterBy : [],
+        duration: this.duration()
+      };
+    },
+
+    standardDuration: function (){
+      if(this.options.duration){
+        return this.options.duration;
+      } else {
+        return Query.prototype.periods[this.options.period].duration;
+      }
+    },
+
+    duration: function (){
+      var maxTimeshift = _.max(this.options.seriesList, function(series){
+        return series.timeshift;
+      });
+      return maxTimeshift.timeshift + this.standardDuration();
+    },
+
+    applyStandardDates: function (seriesList, standard){
+      return _.map(seriesList, function(series){
+        for(var i=0,_i=series.values.length; i<_i; i++){
+          // copy old values by value rather than reference
+          series.values[i]._original_start_at = ""+series.values[i]._start_at;
+          series.values[i]._original_end_at = ""+series.values[i]._end_at;
+
+          series.values[i]._start_at = standard[i]._start_at;
+          series.values[i]._end_at = standard[i]._end_at;
+        }
+        return series;
+      });
+    },
+
+    parse: function (response) {
+      var data = response.data;
+      var startOffset = this.duration() - this.standardDuration();
+      var standardDateValues = data[0].values.slice(startOffset);
+
+      var matchedSeries = _.filter(_.map(this.options.seriesList, function (series) {
+        var dataSeries = _.find(data, function (d) {
+          return d[this.options.category] === series.id;
+        }, this);
+
+        if(dataSeries){
+          var start = startOffset;
+          var id = series.id;
+
+          if(series.timeshift){
+            id = series.id + series.timeshift;
+            start = startOffset - series.timeshift;
+          }
+          var end = start + this.standardDuration();
+
+          return _.extend({}, series, {
+            id: id,
+            values: dataSeries.values.slice(start, end)
+          });
+        }
+        return false;
+      }, this), function(obj){ return obj; }, this);
+
+      return this.applyStandardDates(matchedSeries, standardDateValues);
+    }
+
+  });
+
+  return GroupedTimeshiftCollection;
+});
---
+++ spec/shared/common/collections/spec.grouped_timeshift.js
@@ -0,0 +1,154 @@
+define([
+  'common/collections/grouped_timeshift',
+  'extensions/models/query'
+],
+function (GroupedTimeshiftCollection, Query) {
+  describe("GroupedTimeshiftCollection", function () {
+    var weekDuration = Query.prototype.periods.week.duration;
+
+    it("should return query parameters", function(){
+      var collection = new GroupedTimeshiftCollection([],{
+        valueAttr: 'value',
+        period: 'week',
+        category: 'group',
+        seriesList: [
+          { id: 'one', title: 'One', timeshift: 1 }
+        ]
+      });
+
+      var queryParams = collection.queryParams();
+
+      expect(queryParams.collect).toBe('value');
+      expect(queryParams.period).toBe('week');
+      expect(queryParams.group_by).toBe('group');
+      expect(queryParams.duration).toBe(weekDuration + 1); // standard duration plus timeshift for a week graph
+    });
+
+    it("should calculate standard duration", function(){
+      var collection = new GroupedTimeshiftCollection([],{
+        period: 'week'
+      });
+
+      expect(collection.standardDuration()).toBe(9);
+    });
+
+    it("should allow for a custom standard duration", function(){
+      var collection = new GroupedTimeshiftCollection([],{
+        period: 'week',
+        duration: 29
+      });
+
+      expect(collection.standardDuration()).toBe(29);
+    });
+
+    it("should work out the duration of a graph", function (){
+      var collection = new GroupedTimeshiftCollection([],{
+        period: 'week',
+        seriesList: [
+          { id: 'one', title: 'One', timeshift: 4 },
+          { id: 'two', title: 'Two', timeshift: 5 },
+          { id: 'thr', title: 'Thr', timeshift: 6 },
+        ]
+      });
+
+      expect(collection.duration()).toBe(weekDuration + 6);
+    });
+
+    it("should apply standard dates to a set of values", function(){
+      var seriesList = [
+         { values: [
+            { _start_at: 'a', _end_at: 'b' },
+            { _start_at: 'b', _end_at: 'c' },
+            { _start_at: 'c', _end_at: 'd' }
+          ] },
+         { values: [
+            { _start_at: 'd', _end_at: 'e' },
+            { _start_at: 'e', _end_at: 'f' },
+            { _start_at: 'f', _end_at: 'g' }
+          ] },
+        ];
+      var standardValues = [
+        { _start_at: 'd', _end_at: 'e' },
+        { _start_at: 'e', _end_at: 'f' },
+        { _start_at: 'f', _end_at: 'g' }
+      ]
+      var expectedList = [
+         { values: [
+            { _original_start_at: 'a', _original_end_at: 'b', _start_at: 'd', _end_at: 'e' },
+            { _original_start_at: 'b', _original_end_at: 'c', _start_at: 'e', _end_at: 'f' },
+            { _original_start_at: 'c', _original_end_at: 'd', _start_at: 'f', _end_at: 'g' }
+          ] },
+         { values: [
+            { _original_start_at: 'd', _original_end_at: 'e', _start_at: 'd', _end_at: 'e' },
+            { _original_start_at: 'e', _original_end_at: 'f', _start_at: 'e', _end_at: 'f' },
+            { _original_start_at: 'f', _original_end_at: 'g', _start_at: 'f', _end_at: 'g' }
+          ] },
+        ];
+
+      expect(GroupedTimeshiftCollection.prototype.applyStandardDates(seriesList, standardValues)).toEqual(expectedList);
+    });
+
+    it("should parse response data", function(){
+      var response = {
+        data: [
+          {
+            key: 'one',
+            values: [
+              { _start_at: 'a', _end_at: 'b', value: 1 },
+              { _start_at: 'b', _end_at: 'c', value: 2 },
+              { _start_at: 'c', _end_at: 'd', value: 3 },
+              { _start_at: 'd', _end_at: 'e', value: 4 }
+            ]
+          },
+          {
+            key: 'two',
+            values: [
+              { _start_at: 'a', _end_at: 'b', value: 5 },
+              { _start_at: 'b', _end_at: 'c', value: 6 },
+              { _start_at: 'c', _end_at: 'd', value: 7 },
+              { _start_at: 'd', _end_at: 'e', value: 8 }
+            ]
+          }
+        ]
+      };
+
+      var collection = new GroupedTimeshiftCollection([],{
+        valueAttr: 'value',
+        period: 'week',
+        category: 'key',
+        duration: 2,
+        seriesList: [
+          { id: 'one', title: 'One' },
+          { id: 'one', title: 'One', timeshift: 2 },
+          { id: 'two', title: 'Two' }
+        ]
+      });
+
+      var parsed = collection.parse(response);
+      expect(parsed.length).toBe(3);
+      expect(parsed).toEqual([{
+        id: 'one',
+        title: 'One',
+        values: [
+          { _start_at: 'c', _end_at: 'd', value: 3, _original_start_at: 'c', _original_end_at: 'd' },
+          { _start_at: 'd', _end_at: 'e', value: 4, _original_start_at: 'd', _original_end_at: 'e' }
+        ]
+      },{
+        id: 'one2',
+        title: 'One',
+        timeshift: 2,
+        values: [
+          { _start_at: 'c', _end_at: 'd', value: 1, _original_start_at: 'a', _original_end_at: 'b' },
+          { _start_at: 'd', _end_at: 'e', value: 2, _original_start_at: 'b', _original_end_at: 'c' }
+        ]
+      },{
+        id: 'two',
+        title: 'Two',
+        values: [
+          { _start_at: 'c', _end_at: 'd', value: 7, _original_start_at: 'c', _original_end_at: 'd' },
+          { _start_at: 'd', _end_at: 'e', value: 8, _original_start_at: 'd', _original_end_at: 'e' }
+        ]
+      }]);
+    });
+  });
+});
