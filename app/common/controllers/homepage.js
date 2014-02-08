37a3cf43428576999f6a45db7412118614b54eba
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-02-08 12:33:18 +0000

    Add homepage
    I've made a couple of changes while moving it across:
    
    - Change link to blog and link text to be correct
    - Remove live GOV.UK usage module (should be easy to re-add?)
    - Add height and width to Transactions Explorer image, make it a link

---
+++ app/common/controllers/homepage.js
@@ -0,0 +1,101 @@
+define([
+  'backbone',
+  'page_config',
+  'extensions/controllers/controller',
+  'common/views/homepage'
+],
+function (Backbone, PageConfig, Controller, HomepageView) {
+
+  var HomepageController = Controller.extend({
+    viewClass: HomepageView
+  });
+
+  var homepage = function (req, res, next) {
+    var model = new Backbone.Model();
+
+    model.set(PageConfig.commonConfig(req));
+
+    // We should fetch this information from Stagecraft!
+    var detailedDashboardColumns = [
+      [
+        {
+          title: 'C',
+          services: [
+            {name: "Carer's Allowance", slug: "carers-allowance"}
+          ]
+        },
+        {
+          title: 'D',
+          services: [
+            {name: "Deposit foreign marriage or civil partnership certificates", slug: "deposit-foreign-marriage"}
+          ]
+        },
+        {
+          title: 'G',
+          services: [
+            {name: "G-Cloud", slug: "g-cloud"}
+          ]
+        },
+        {
+          title: 'L',
+          services: [
+            {name: "Lasting Power of Attorney", slug: "lasting-power-of-attorney"},
+            {name: "Licensing", slug: "licensing"}
+          ]
+        }
+      ],
+      [
+        {
+          title: 'P',
+          services: [
+            {name: 'Pay to get documents legalised by post', slug: 'pay-legalisation-post'},
+            {name: 'Pay to legalise documents using the premium service', slug: 'pay-legalisation-drop-off'},
+            {name: 'Payment for certificates to get married abroad', slug: 'pay-foreign-marriage-certificates'},
+            {name: 'Payment to register a birth abroad in the UK', slug: 'pay-register-birth-abroad'},
+            {name: 'Payment to register a death abroad', slug: 'pay-register-death-abroad'}
+          ]
+        }
+      ],
+      [
+        {
+          title: 'S',
+          services: [
+            {name: 'SORN (Statutory Off Road Notification)', slug: 'sorn'}
+          ]
+        },
+        {
+          title: 'T',
+          services: [
+            {name: "Tax disc", slug: "tax-disc"}
+          ]
+        },
+        {
+          title: 'Service groups',
+          services: [
+            {name: "Vehicle licensing", slug: "vehicle-licensing"}
+          ]
+        }
+      ]
+    ];
+
+    _.each(detailedDashboardColumns, function (column) {
+      _.each(column, function (group) {
+        group.sanitised_title = group.title.toLowerCase().replace(' ', '_');
+      });
+    });
+
+    model.set('detailedDashboardColumns', detailedDashboardColumns);
+
+    var homepageController = new HomepageController({
+      model: model,
+      raw: req.query.raw,
+      url: req.originalUrl
+    });
+
+    homepageController.render();
+
+    res.send(homepageController.html);
+  };
+
+  return homepage;
+});
