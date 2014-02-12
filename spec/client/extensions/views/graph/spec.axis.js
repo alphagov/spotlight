cf5dabaa5edff06db0e8c6f321def0b7e4be209a
Anna Powell-Smith <anna.powell-smith@digital.cabinet-office.gov.uk>
2014-02-12 12:13:03 +0000

    Allow array of arguments for axis config options.
    This is to allow the "ticks" option to pass more than one argument if required.

--- spec/client/extensions/views/graph/spec.axis.js
+++ spec/client/extensions/views/graph/spec.axis.js
@@ -2,19 +2,19 @@ define([
   'extensions/views/graph/axis'
 ],
 function (Axis) {
-  
+
   describe("render", function() {
-    
+
     var el, wrapper;
     beforeEach(function() {
       el = $('<div></div>').appendTo($('body'));
       wrapper = Axis.prototype.d3.select(el[0]).append('svg').append('g');
     });
-    
+
     afterEach(function() {
       el.remove();
     });
-    
+
     it("requires a scale", function() {
       expect(function () {
         var view = new Axis({
@@ -23,9 +23,9 @@ function (Axis) {
         view.render()
       }).toThrow();
     });
-    
+
     it("renders a d3 axis component", function() {
-      
+
       var view = new Axis({
         collection: {
           on: jasmine.createSpy()
@@ -41,9 +41,9 @@ function (Axis) {
         }
       });
       spyOn(view.d3.svg, "axis").andCallThrough();
-      
+
       view.render()
-      
+
       expect(wrapper.selectAll('.tick')[0].length).toEqual(11);
     });
 
@@ -61,19 +61,18 @@ function (Axis) {
           innerWidth: 100,
           innerHeight: 100
         },
-        tickValues: [2,4,6]
+        tickValues: [[2,4,6]]
       });
       spyOn(view.d3.svg, "axis").andCallThrough();
-      
-      view.render()
-      
+
+      view.render();
       var ticks = wrapper.selectAll('.tick')[0];
       expect(wrapper.selectAll('.tick')[0].length).toEqual(3);
       expect(d3.select(ticks[0]).text()).toEqual('2.0');
       expect(d3.select(ticks[1]).text()).toEqual('4.0');
       expect(d3.select(ticks[2]).text()).toEqual('6.0');
 
-      view.tickValues = [1,2,3];
+      view.tickValues = [[1,2,3]];
       view.render();
 
       var ticks = wrapper.selectAll('.tick')[0];
@@ -81,11 +80,11 @@ function (Axis) {
       expect(d3.select(ticks[1]).text()).toEqual('2.0');
       expect(d3.select(ticks[2]).text()).toEqual('3.0');
     });
-    
+
   });
-  
+
   describe("getTransform", function() {
-    
+
     var view;
     beforeEach(function() {
       view = new Axis({
@@ -98,55 +97,55 @@ function (Axis) {
         }
       });
     });
-    
+
     it("calculates default translation for position left", function() {
       view.position = 'left';
       expect(view.getTransform()).toEqual('translate(0,0)');
     });
-    
+
     it("calculates default translation for position right", function() {
       view.position = 'right';
       expect(view.getTransform()).toEqual('translate(555,0)');
     });
-    
+
     it("calculates default translation for position top", function() {
       view.position = 'top';
       expect(view.getTransform()).toEqual('translate(0,0)');
     });
-    
+
     it("calculates default translation for position bottom", function() {
       view.position = 'bottom';
       expect(view.getTransform()).toEqual('translate(0,444)');
     });
-    
+
     it("calculates custom translation for position left", function() {
       view.position = 'left';
       view.offsetX = 7;
       view.offsetY = 8;
       expect(view.getTransform()).toEqual('translate(7,8)');
     });
-    
+
     it("calculates custom translation for position right", function() {
       view.position = 'right';
       view.offsetX = 7;
       view.offsetY = 8;
       expect(view.getTransform()).toEqual('translate(562,8)');
     });
-    
+
     it("calculates custom translation for position top", function() {
       view.position = 'top';
       view.offsetX = 7;
       view.offsetY = 8;
       expect(view.getTransform()).toEqual('translate(7,8)');
     });
-    
+
     it("calculates custom translation for position bottom", function() {
       view.position = 'bottom';
       view.offsetX = 7;
       view.offsetY = 8;
       expect(view.getTransform()).toEqual('translate(7,452)');
     });
-    
+
   });
-  
+
 });
