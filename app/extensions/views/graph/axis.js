00ddad75c6a2448cec8c579b805b5aa8d927afd6
Anna Powell-Smith <anna.powell-smith@digital.cabinet-office.gov.uk>
2014-02-12 12:13:03 +0000

    Allow array of arguments for axis config options.
    This is to allow the "ticks" option to pass more than one argument if required.

--- app/extensions/views/graph/axis.js
+++ app/extensions/views/graph/axis.js
@@ -3,30 +3,30 @@ define([
 ],
 function (Component) {
   var Axis = Component.extend({
-    
+
     position: 'bottom',
     tickPadding: 0,
     orient: 'bottom',
     classed: null,
-    
+
     // Not implemented; override in configuration or subclass
     getScale: function () {
       throw('No scale defined.');
     },
-    
+
     render: function () {
       Component.prototype.render.apply(this, arguments);
-      
+
       var scale = this.getScale();
-      
+
       var axis = this.d3.svg.axis()
         .scale(scale)
         .orient(this.orient);
 
-      
       _.each(['ticks', 'tickValues', 'tickFormat', 'tickPadding', 'tickSize'], function (id) {
         if (this[id] != null) {
-          axis[id](_.isFunction(this[id]) ? this[id]() : this[id]);
+          var args = _.isFunction(this[id]) ? this[id]() : this[id];
+          axis[id].apply(this, _.isArray(args) ? args : [args]);
         }
       }, this);
 
@@ -39,17 +39,17 @@ function (Component) {
         return a < b ? -1 : a > b ? 1 : 0;
       });
     },
-    
+
     getTransform: function () {
       var offsetX = this.offsetX || 0;
       var offsetY = this.offsetY || 0;
-      
+
       if (this.position == 'right') {
         offsetX += this.graph.innerWidth;
       } else if (this.position === 'bottom') {
         offsetY += this.graph.innerHeight;
       }
-      
+
       return "translate(" + offsetX + "," + offsetY + ")";
     }
 
