2812d34c0b4954cb53d428055b7b338942eb98bc
Tom Booth <tombooth@gmail.com>
2014-02-17 17:15:03 +0000

    VisitorsRealtimeCollection -> ListCollection
    We are going to need basically the same functionality for gathering and
    displaying data for the ListView, so rather than writing something new I
    thought I would generify this class.
    
    All functionality as expected by visitors-realtime should have remained
    the same, I have adapted and not removed any of the tests.

---
+++ app/common/collections/list.js
@@ -0,0 +1,62 @@
+define([
+  'extensions/collections/matrix'
+],
+function (MatrixCollection) {
+  var ListCollection = MatrixCollection.extend({
+
+    queryParams: function () {
+      return {
+        sort_by: this.options.sortBy,
+        limit: this.options.limit
+      };
+    },
+
+    initialize: function (models, options) {
+      MatrixCollection.prototype.initialize.apply(this, arguments);
+
+      // this is delibrately not allowing empty strings
+      // as this is an undesirable title or id
+      if (!options || !options.title || !options.id) {
+        throw new Error("Both 'title' and 'id' are required options for a ListCollection instance");
+      }
+
+      if (isClient && _.isNumber(this.options.updateInterval)) {
+        clearInterval(this.timer);
+        this.timer = setInterval(
+          _.bind(this.fetch, this), this.options.updateInterval
+        );
+      }
+    },
+
+    parse: function (response) {
+
+      var numberRegexp = /^[0-9\.]+$/,
+          data;
+
+      data = _.map(response.data, function(d) {
+        return _.reduce(d, function(out, val, key) {
+          if (numberRegexp.test(val)) {
+            val = parseFloat(val);
+          }
+          out[key] = val;
+          return out;
+        }, {});
+      });
+
+      return {
+        id: this.options.id,
+        title: this.options.title,
+        values: data.reverse()
+      };
+
+    },
+
+    fetch: function (options) {
+      options = _.extend(this.options.fetchOptions || {}, options);
+      MatrixCollection.prototype.fetch.call(this, options);
+    }
+
+  });
+
+  return ListCollection;
+});
