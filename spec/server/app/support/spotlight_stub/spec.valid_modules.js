9c2bd26e822278822b502f562ea753523ca4e7bf
Chris Tarttelin <ctarttelin@equalexperts.com>
2014-02-18 16:07:40 +0000

    Add disabled schema and spec

---
+++ spec/server/app/support/spotlight_stub/spec.valid_modules.js
@@ -0,0 +1,16 @@
+define(['fs', 'jsonschema'], function(fs, jsonschema) {
+  describe('spotlight modules', function() {
+    var module_schema = JSON.parse(fs.readFileSync('spec/module-schema.json'));
+    var modules = fs.readdirSync('app/support/stagecraft_stub/responses/student-finance');
+    modules.forEach(function(fileName) {
+      describe('student-finance module ' + fileName, function() {
+        xit('should conform to the schema definition', function() {
+          var module_to_test = JSON.parse(fs.readFileSync(path.join('app/support/stagecraft_stub/responses/student-finance', fileName)));
+          var result = jsonschema.validate(module_to_test, module_schema);
+          expect(result.valid).toBeTruthy();
+        });
+
+      });
+    });
+  });
+});
