2c91cbfc1cdb245093d0350e9a26120e26e62853
Ben Marvell <madmarv187@gmail.com>
2014-02-18 14:25:39 +0000

    Fixing lint errors.

--- spec/shared/common/collections/spec.visitors-realtime.js
+++ spec/shared/common/collections/spec.visitors-realtime.js
@@ -3,43 +3,43 @@ define([
   'extensions/collections/collection'
 ],
 function (VisitorsRealtimeCollection, Collection) {
-  describe("VisitorsRealtimeCollection", function () {
+  describe('VisitorsRealtimeCollection', function () {
 
-    beforeEach(function() {
+    beforeEach(function () {
       jasmine.Clock.useMock();
-      spyOn(VisitorsRealtimeCollection.prototype, "fetch");
-      spyOn(Collection.prototype, "fetch");
+      spyOn(VisitorsRealtimeCollection.prototype, 'fetch');
+      spyOn(Collection.prototype, 'fetch');
     });
 
-    it("is created with the correct query parameters", function () {
+    it('is created with the correct query parameters', function () {
       var collection = new VisitorsRealtimeCollection(
         { 'data' : [] },
         { 'numTwoMinPeriodsToQuery': 60 }
       );
       var params = collection.queryParams();
-      expect(params.sort_by).toEqual("_timestamp:descending");
+      expect(params.sort_by).toEqual('_timestamp:descending');
       expect(params.limit).toEqual(60);
     });
 
-    it("parses string data into float format", function () {
+    it('parses string data into float format', function () {
       var collection = new VisitorsRealtimeCollection({
           'data': [
             {
-              _timestamp: "2002-03-01T00:00:03+00:00",
+              _timestamp: '2002-03-01T00:00:03+00:00',
               unique_visitors: '100'
             },
             {
-              _timestamp: "2002-03-01T00:00:00+00:00",
+              _timestamp: '2002-03-01T00:00:00+00:00',
               unique_visitors: '120'
             }
           ]
-      }, { 'parse': true } );
-      expect(collection.first().get('values').first().get('_timestamp').format()).toEqual("2002-03-01T00:00:00+00:00");
+      }, { 'parse': true });
+      expect(collection.first().get('values').first().get('_timestamp').format()).toEqual('2002-03-01T00:00:00+00:00');
       expect(collection.first().get('values').first().get('unique_visitors')).toEqual(120);
       expect(collection.first().get('values').last().get('unique_visitors')).toEqual(100);
     });
 
-    it("auto-updates on the client", function () {
+    it('auto-updates on the client', function () {
       jasmine.clientOnly(function () {
         var collection = new VisitorsRealtimeCollection();
         expect(collection.fetch).not.toHaveBeenCalled();
@@ -50,7 +50,7 @@ function (VisitorsRealtimeCollection, Collection) {
       });
     });
 
-    it("does not auto-update on the server", function () {
+    it('does not auto-update on the server', function () {
       jasmine.serverOnly(function () {
         var collection = new VisitorsRealtimeCollection();
         expect(collection.fetch).not.toHaveBeenCalled();
@@ -59,14 +59,14 @@ function (VisitorsRealtimeCollection, Collection) {
       });
     });
 
-    it("sets cache headers correctly", function () {
+    it('sets cache headers correctly', function () {
       var collection = new VisitorsRealtimeCollection();
       collection.fetch.andCallThrough();
       collection.fetch();
       expect(Collection.prototype.fetch).toHaveBeenCalled();
       var args = Collection.prototype.fetch.argsForCall[0][0];
       expect(args).toEqual({
-        headers: {"cache-control": "max-age=120"}
+        headers: {'cache-control': 'max-age=120'}
       });
     });
   });
