e2b3cf0b4fd59c42c4f99e17d7a16403679ba62b
Ben Marvell <madmarv187@gmail.com>
2014-02-21 15:44:02 +0000

    Fixed linting and trailing spaces in this file

--- app/common/collections/multi_stats.js
+++ app/common/collections/multi_stats.js
@@ -3,20 +3,20 @@ define([
 ],
 function (MatrixCollection) {
   var MultiStatsCollection = MatrixCollection.extend({
-    
+
     initialize: function (attrs, options) {
       options = options || {};
       this.stats = options.stats;
       this.period = options.period;
       MatrixCollection.prototype.initialize.apply(this, arguments);
     },
-    
-    getAttrNames: function() {
+
+    getAttrNames: function () {
       return _.map(this.stats, function (stat) {
-       return stat.attr; 
+       return stat.attr;
       });
     },
-    
+
     queryParams: function () {
       return {
         period: this.period,
@@ -31,27 +31,27 @@ function (MatrixCollection) {
           _start_at: item._start_at,
           _end_at: item._end_at
         };
-        
+
         _.each(this.getAttrNames(), function (attr) {
           if (item[attr] && item[attr].length) {
             latestIndexWithValues = index;
             res[attr] = item[attr][0];
           }
         }, this);
-        
+
         return res;
       }, this);
 
       data = data.slice(0, latestIndexWithValues + 1);
-      
+
       return {
         id: 'multistats',
         title: 'MultiStats',
         values: data
       };
-      
+
     }
-    
+
   });
   return MultiStatsCollection;
 });
