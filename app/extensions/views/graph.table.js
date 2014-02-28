8a9890b9ad7ce271f150578964054b70bdb8d0a5
Ben Marvell <madmarv187@gmail.com>
2014-02-28 14:08:12 +0000

    New graph table view.
    This takes a table and uses the showHide view to make the table visible.

---
+++ app/extensions/views/graph.table.js
@@ -0,0 +1,24 @@
+define([
+  './table',
+  './hideShow'
+],
+function (Table, HideShow) {
+  return Table.extend({
+    initialize: function () {
+      this.$toggleContainer = $('<div></div>');
+
+      Table.prototype.initialize.apply(this, arguments);
+
+      this.toggleTable = new HideShow({
+        $reveal: this.$table,
+        $el: this.$toggleContainer,
+        showLabel: 'Show the data for this graph.',
+        hideLabel: 'Hide the data for this graph.'
+      });
+    },
+    prepareTable: function () {
+      this.$table.appendTo(this.$toggleContainer);
+      this.$toggleContainer.appendTo(this.$el);
+    },
+  });
+});
