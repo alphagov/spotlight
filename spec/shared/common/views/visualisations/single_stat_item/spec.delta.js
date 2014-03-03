7ae9d00a9ebb89082a91f0194708d30ea932a800
Anna Powell-Smith <anna.powell-smith@digital.cabinet-office.gov.uk>
2014-03-03 15:11:32 +0000

    Fix JSLint errors

--- spec/shared/common/views/visualisations/single_stat_item/spec.delta.js
+++ spec/shared/common/views/visualisations/single_stat_item/spec.delta.js
@@ -1,88 +1,87 @@
 define([
   'common/views/visualisations/multi_stat_item/single_stat_item/delta',
   'extensions/models/model',
-  'extensions/collections/collection',
-  'Mustache'
+  'extensions/collections/collection'
 ],
-function (DeltaView, Model, Collection, Mustache) {
-  describe("DeltaView", function () {
+function (DeltaView, Model, Collection) {
+  describe('DeltaView', function () {
   
-  var collection, view;
-  beforeEach(function() { 
-    collection = new Collection();
-    collection.reset([ {
-      id: 'test',
-      title: 'test',
-      values: new Collection([
-        {
-          _start_at: collection.getMoment("2012-09-01T00:00:00+00:00"),
-          a: 1, 
-          b: 2,
-          c: null,
-          d: 0,
-          e: 5
+    var collection, view;
+    beforeEach(function () {
+      collection = new Collection();
+      collection.reset([ {
+        id: 'test',
+        title: 'test',
+        values: new Collection([
+          {
+            _start_at: collection.getMoment("2012-09-01T00:00:00+00:00"),
+            a: 1,
+            b: 2,
+            c: null,
+            d: 0,
+            e: 5
+          },
+          {
+            _start_at: collection.getMoment("2013-09-01T00:00:00+00:00"),
+            a: 0.5,
+            b: 4,
+            c: 6,
+            d: 10,
+            e: 0
+          }
+        ])
+      } ]);
+    
+      view = new DeltaView({
+        collection: collection,
+        stat: {
+          'title': 'Statistic A',
+          'attr': 'a'
         },
-        { 
-          _start_at: collection.getMoment("2013-09-01T00:00:00+00:00"),
-          a: 0.5,
-          b: 4,
-          c: 6,
-          d: 10,
-          e: 0
-        }
-      ])
-    } ]);
-    
-    view = new DeltaView({
-      collection: collection,
-      stat: {
-        "title": "Statistic A",
-        "attr": "a"
-      },
-      valueAttr: "a"
+        valueAttr: 'a'
+      });
+      
     });
-    
-  });
   
-  it("renders sample data", function () {
+    it('renders sample data', function () {
 
-    jasmine.renderView(view, function () {
-      expect(view.$el.find('.change')).toHaveText('−50.00%');
-      expect(view.$el.text()).toContain('Sep 2012');
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('.change')).toHaveText('−50.00%');
+        expect(view.$el.text()).toContain('Sep 2012');
+      });
+
     });
-
-  });
   
-  it("correctly applies increase and decrease classes to the number", function () {
+    it('correctly applies increase and decrease classes to the number', function () {
 
-    jasmine.renderView(view, function () {
-      expect(view.$el.find('.change')).toHaveClass('decrease');
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('.change')).toHaveClass('decrease');
+      });
+
     });
 
-  });
-
-  
-  it("does not show a delta if the denominator is null", function () {
-    collection.first().get('values').first().set('a', null);
-    jasmine.renderView(view, function () {
-      expect(view.$el.find('span')).toHaveClass('no-data');
+    it('does not show a delta if the denominator is null', function () {
+      collection.first().get('values').first().set('a', null);
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('span')).toHaveClass('no-data');
+      });
     });
-  });
 
-  it("does not show a delta if the denominator is zero", function () {
-    
-    collection.first().get('values').first().set('a', 0);
-    jasmine.renderView(view, function () {
-      expect(view.$el.find('span')).toHaveClass('no-data');
+    it('does not show a delta if the denominator is zero', function () {
+      
+      collection.first().get('values').first().set('a', 0);
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('span')).toHaveClass('no-data');
+      });
     });
-  });
 
-  it("does show a delta if the numerator is zero", function () {
-    collection.first().get('values').last().set('a', 0);
-    jasmine.renderView(view, function () {
-      expect(view.$el.find('.change').text()).toContain('−100.00%');
+    it('does show a delta if the numerator is zero', function () {
+      collection.first().get('values').last().set('a', 0);
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('.change').text()).toContain('−100.00%');
+      });
     });
-  });
  
- }); 
+  });
+
 });
--- spec/shared/common/views/visualisations/single_stat_item/spec.headline.js
+++ spec/shared/common/views/visualisations/single_stat_item/spec.headline.js
@@ -1,75 +1,75 @@
 define([
   'extensions/views/single_stat',
   'common/views/visualisations/multi_stat_item/single_stat_item/headline',
-  'extensions/collections/collection',
-  'Mustache'
+  'extensions/collections/collection'
 ],
-function (SingleStatView, HeadlineView, Collection, Mustache) {
-  describe("HeadlineView", function () {
+function (SingleStatView, HeadlineView, Collection) {
+  describe('HeadlineView', function () {
 
-  var collection, view;
-  beforeEach(function() { 
-    collection = new Collection();
-    collection.reset([ {
-      id: 'test',
-      title: 'test',
-      values: new Collection([
-        {
-          _start_at: collection.getMoment("2013-08-01T00:00:00+00:00"),
-          a: 1, 
-          b: 2,
-          c: null,
-          d: 0,
-          e: 5
+    var collection, view;
+    beforeEach(function () {
+      collection = new Collection();
+      collection.reset([ {
+        id: 'test',
+        title: 'test',
+        values: new Collection([
+          {
+            _start_at: collection.getMoment('2013-08-01T00:00:00+00:00'),
+            a: 1,
+            b: 2,
+            c: null,
+            d: 0,
+            e: 5
+          },
+          {
+            _start_at: collection.getMoment('2013-09-01T00:00:00+00:00'),
+            a: 0.5,
+            b: 4,
+            c: 6,
+            d: 10,
+            e: 0
+          }
+        ])
+      } ]);
+      view = new HeadlineView({
+        collection: collection,
+        stat: {
+          'title': 'Statistic A',
+          'attr': 'a'
         },
-        { 
-          _start_at: collection.getMoment("2013-09-01T00:00:00+00:00"),
-          a: 0.5,
-          b: 4,
-          c: 6,
-          d: 10,
-          e: 0
-        }
-      ])
-    } ]);
-    view = new HeadlineView({
-      collection: collection,
-      stat: {
-        "title": "Statistic A",
-        "attr": "a"
-      },
-      valueAttr: "a"
+        valueAttr: 'a'
+      });
     });
-  });
   
-  it("renders sample data", function () {
+    it('renders sample data', function () {
 
-    jasmine.renderView(view, function () {
-      expect(view.$el.find('strong')).toHaveText('0.5');
-      expect(view.$el.text()).toContain('Sep 2013');
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('strong')).toHaveText('0.5');
+        expect(view.$el.text()).toContain('Sep 2013');
+      });
+
     });
 
-  });
-
-  it("indicates missing data", function () {
-    
-    collection.first().get('values').last().set('a', null);
-    jasmine.renderView(view, function () {
-      expect(view.$el.find('strong').length).toEqual(0);
-      expect(view.$el.text()).toContain('(no data)');
+    it('indicates missing data', function () {
+      
+      collection.first().get('values').last().set('a', null);
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('strong').length).toEqual(0);
+        expect(view.$el.text()).toContain('(no data)');
+      });
+      
     });
     
-  });
-  
-  it("indicates missing data", function () {
-    
-    collection.first().get('values').last().set('a', null);
-    jasmine.renderView(view, function () {
-      expect(view.$el.find('strong').length).toEqual(0);
-      expect(view.$el.text()).toContain('(no data)');
+    it('indicates missing data', function () {
+      
+      collection.first().get('values').last().set('a', null);
+      jasmine.renderView(view, function () {
+        expect(view.$el.find('strong').length).toEqual(0);
+        expect(view.$el.text()).toContain('(no data)');
+      });
+      
     });
-    
-  });
  
- }); 
+  });
+
 });
