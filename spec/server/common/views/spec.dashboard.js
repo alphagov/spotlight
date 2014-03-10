2a553665c7e0a37162b4b2e851e186f08c8c03ca
annapowellsmith <annapowellsmith@gmail.com>
2014-03-10 21:06:31 +0000

    Update dashboards for new dashboard design.
    New dashboard design includes multiple changes, and introduces concept
    of dashboard type - we now have department pages (e.g. Department for
    Work and Pensions), agency pages (e.g. Pensions Ombudsman, part of the
    DWP), service pages (e.g. Carer's Allowance, with applications, renewals
    etc), transaction pages (e.g. Carer's Allowance applications) and
    finally policy pages (e.g. Housing).
    
    We currently only have designs for transaction and policy pages: the
    other dashboard types will essentially be lists of transactions.
    
    The page title, page header, tagline, breadcrumbs, and related pages on
    the dashboard will vary dependent on the dashboard type. This commit
    defines how they vary, and updates the template accordingly.
    
    We now have the following dashboard-specific variables:
    
    * Page header. This is simple except for transactions, where the page
      header is the name of both the service and the transaction.
    * Page title, i.e. <title> tag. This includes the strapline if defined.
    * Tagline. This varies with dashboard type. Policy dashboards retain
      their existing taglines.
    * Breadcrumbs, defined in getBreadcrumbCrumbs. The agency breadcrumb is
      included if an agency is defined for this service. (Note that this
      function isn't particularly elegant, but if I rewrite it to be more
      concise, I get lint warnings about function complexity.) These
      breadcrumbs are not linked anywhere yet, since we don't yet have
      designs for department, agency or service dashboards.
    * Related pages. These are similar to the existing related pages,
      with one exception: if there is a link to a related service, it is
      styled differently.
    
    Tests also updated.

--- spec/server/common/views/spec.dashboard.js
+++ spec/server/common/views/spec.dashboard.js
@@ -8,7 +8,11 @@ function (DashboardView, Model) {
     var view, model;
     beforeEach(function () {
       model = new Model({
-        foo: 'bar'
+        foo: 'bar',
+        dashboardType: 'service',
+        service: {
+          'title': 'Carer\'s Allowance'
+        }
       });
       view = new DashboardView({
         model: model,
@@ -31,21 +35,195 @@ function (DashboardView, Model) {
       });
     });
 
+    describe('getPageHeader', function () {
+
+      it('calculates correct page header for services', function () {
+        model.set({
+          service: {
+            title: 'Carer\'s Allowance'
+          }
+        });
+        view.dashboardType = 'service';
+        expect(view.getPageHeader()).toEqual('Carer\'s Allowance');
+      });
+
+      it('calculates correct page header for transactions', function () {
+        model.set({
+          service: {
+            title: 'Carer\'s Allowance'
+          },
+          transaction: {
+            title: 'applications'
+          }
+        });
+        view.dashboardType = 'transaction';
+        expect(view.getPageHeader()).toEqual('Carer\'s Allowance applications');
+      });
+
+    });
+
+
+    describe('getTagline', function () {
+
+      it('calculates correct tagline for departments', function () {
+        model.set({
+          dashboardType: 'department',
+          department: {
+            title: 'Department for Work and Pensions'
+          }
+        });
+        view.dashboardType = 'department';
+        expect(view.getTagline()).toEqual('This dashboard shows information about how selected services run by the <strong>Department for Work and Pensions</strong> are currently performing.');
+      });
+
+      it('calculates correct tagline for agencies', function () {
+        model.set({
+          agency: {
+            title: 'Pensions Ombudsman'
+          }
+        });
+        view.dashboardType = 'agency';
+        expect(view.getTagline()).toEqual('This dashboard shows information about how selected services run by the <strong>Pensions Ombudsman</strong> are currently performing.');
+      });
+
+      it('calculates correct tagline for services', function () {
+        model.set({
+          service: {
+            title: 'Carer\'s Allowance'
+          },
+          transaction: {
+            title: 'applications'
+          }
+        });
+        view.dashboardType = 'service';
+        expect(view.getTagline()).toEqual('This dashboard shows information about how the <strong>Carer\'s Allowance</strong> service is currently performing.');
+      });
+
+      it('calculates correct tagline for transactions', function () {
+        model.set({
+          service: {
+            title: 'Carer\'s Allowance'
+          },
+          transaction: {
+            title: 'applications'
+          }
+        });
+        view.dashboardType = 'transaction';
+        expect(view.getTagline()).toEqual('This dashboard shows information about how the <strong>Carer\'s Allowance applications</strong> service is currently performing.');
+      });
+
+      it('calculates correct tagline for policies', function () {
+        model.set({
+          policy: {
+            title: 'Housing',
+            tagline: 'The government is helping local councils and developers work with local communities to plan and build better places to live for everyone.'
+          }
+        });
+        view.dashboardType = 'policy';
+        expect(view.getTagline()).toEqual('The government is helping local councils and developers work with local communities to plan and build better places to live for everyone.');
+      });
+
+    });
+
     describe('getPageTitle', function () {
+
       it('calculates page title from title and strapline', function () {
         model.set({
-          title: 'Title',
-          'strapline': 'Strapline'
+          service: {
+            title: 'Title'
+          },
+          'strapline': 'Service dashboard'
         });
-        expect(view.getPageTitle()).toEqual('Title - Strapline - GOV.UK');
+        view.dashboardType = 'service';
+        expect(view.getPageTitle()).toEqual('Title - Service dashboard - GOV.UK');
       });
 
       it('calculates page title from title alone', function () {
         model.set({
-          title: 'Title'
+          service: {
+            title: 'Title'
+          }
         });
+        view.dashboardType = 'service';
         expect(view.getPageTitle()).toEqual('Title - Performance - GOV.UK');
       });
     });
+
+    describe('getBreadcrumbCrumbs', function () {
+
+      it('calculates correct crumbs for departments', function () {
+        model.set({
+          dashboardType: 'department',
+          department: {
+            title: 'Department for Work and Pensions'
+          }
+        });
+        view.dashboardType = 'department';
+        expect(view.getBreadcrumbCrumbs()).toEqual([
+          {'path': '/performance', 'title': 'Performance'}
+        ]);
+      });
+
+      it('calculates correct crumbs for agencies', function () {
+        model.set({
+          department: {
+            title: 'Department for Work and Pensions'
+          }
+        });
+        view.dashboardType = 'agency';
+        expect(view.getBreadcrumbCrumbs()).toEqual([
+          {'path': '/performance', 'title': 'Performance'},
+          {'title': 'Department for Work and Pensions'}
+        ]);
+      });
+
+      it('calculates correct crumbs for services, including agency if defined', function () {
+        model.set({
+          department: {
+            title: 'Department for Work and Pensions'
+          },
+          agency: {
+            title: 'Pensions Ombudsman'
+          }
+        });
+        view.dashboardType = 'service';
+        expect(view.getBreadcrumbCrumbs()).toEqual([
+          {'path': '/performance', 'title': 'Performance'},
+          {'title': 'Department for Work and Pensions'},
+          {'title': 'Pensions Ombudsman'}
+        ]);
+      });
+
+      it('calculates correct crumbs for transactions', function () {
+        model.set({
+          department: {
+            title: 'Department for Work and Pensions'
+          },
+          service: {
+            title: 'Carer\'s Allowance'
+          }
+        });
+        view.dashboardType = 'transaction';
+        expect(view.getBreadcrumbCrumbs()).toEqual([
+          {'path': '/performance', 'title': 'Performance'},
+          {'title': 'Department for Work and Pensions'},
+          {'title': 'Carer\'s Allowance'}
+        ]);
+      });
+
+      it('calculates correct crumbs for policies', function () {
+        model.set({
+          policy: {
+            title: 'Housing',
+            tagline: 'The government is helping local councils and developers work with local communities to plan and build better places to live for everyone.'
+          }
+        });
+        view.dashboardType = 'policy';
+        expect(view.getBreadcrumbCrumbs()).toEqual([
+          {'path': '/performance', 'title': 'Performance'}
+        ]);
+      });
+    });
+
   });
 });
