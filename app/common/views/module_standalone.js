84519ae160018291833724d285a1e2615a1fe969
annapowellsmith <annapowellsmith@gmail.com>
2014-03-10 22:52:39 +0000

    Use dashboard-slug if specified in module stub.
    This is so that we don't lose functionality for existing dashboards
    which do not fit nicely into the department/agency/service hierarchy.
    Namely, the G-Cloud and site activity dashboards.
    
    We may want to look at a better way to manage this in future, but
    for the moment, use dashboard-slug if it is defined.

--- app/common/views/module_standalone.js
+++ app/common/views/module_standalone.js
@@ -32,14 +32,21 @@ function (GovUkView, Module) {
       var crumbs = [
         {path: '/performance', title: 'Performance'}
       ];
-      var crumbNames = ['department', 'agency', 'service', 'policy'];
-      _.each(crumbNames, function (cn) {
-        if (this.model.get(cn)) {
-          crumbs.push({
-            'title': this.model.get(cn).title
-          });
-        }
-      }, this);
+      if (this.model.get('dashboard-slug') && this.model.get('dashboard-title')) {
+        crumbs.push({
+          path: '/performance/' + this.model.get('dashboard-slug'),
+          title: this.model.get('dashboard-title')
+        });
+      } else {
+        var crumbNames = ['department', 'agency', 'service', 'policy'];
+        _.each(crumbNames, function (cn) {
+          if (this.model.get(cn)) {
+            crumbs.push({
+              'title': this.model.get(cn).title
+            });
+          }
+        }, this);
+      }
       return crumbs;
     }
   });
