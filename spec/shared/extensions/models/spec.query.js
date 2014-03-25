ab9350b177bb41de7a6f9815f882aea820fe0a0e
annapowellsmith <annapowellsmith@gmail.com>
2014-03-25 14:36:39 +0000

    Allow pinning of start and end dates in query.
    We want to allow users to 'pin' the start and/or end date of a
    query.
    
    Add options in query.js to allow this.
    
    Note that the start and end dates are only set if the 'period'
    parameter is also set: this replicates the existing logic.
    
    Update tests.

--- spec/shared/extensions/models/spec.query.js
+++ spec/shared/extensions/models/spec.query.js
@@ -151,6 +151,41 @@ function (Query) {
         expect(query.get('start_at').format()).toEqual('2005-04-01T00:00:00+00:00');
       });
 
+      it('uses explicit start and end dates where defined', function () {
+        var query = new Query({
+          foo: 'bar',
+          period: 'month',
+          start_at: '2013-01-01T00:00:00+00:00',
+          end_at: '2014-01-01T00:00:00+00:00'
+        });
+        expect(query.get('foo')).toEqual('bar');
+        expect(query.get('period')).toEqual('month');
+        expect(query.get('start_at').format()).toEqual('2013-01-01T00:00:00+00:00');
+        expect(query.get('end_at').format()).toEqual('2014-01-01T00:00:00+00:00');
+      });
+
+      it('uses an explicit start date where defined, and defaults the end date to now', function () {
+        var query = new Query({
+          foo: 'bar',
+          period: 'month',
+          start_at: '2013-01-01T00:00:00+00:00'
+        });
+        expect(query.get('foo')).toEqual('bar');
+        expect(query.get('start_at').format()).toEqual('2013-01-01T00:00:00+00:00');
+        expect(query.get('end_at').format()).toEqual('2013-05-01T00:00:00+00:00');
+      });
+
+      it('uses an explicit end date where defined, and defaults to a start date as defined by the period', function () {
+        var query = new Query({
+          foo: 'bar',
+          period: 'month',
+          end_at: '2014-01-01T00:00:00+00:00'
+        });
+        expect(query.get('foo')).toEqual('bar');
+        expect(query.get('start_at').format()).toEqual('2013-01-01T00:00:00+00:00');
+        expect(query.get('end_at').format()).toEqual('2014-01-01T00:00:00+00:00');
+      });
+
     });
   });
 });
