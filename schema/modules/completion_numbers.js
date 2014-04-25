cd318a16a8c9f707f73d96290c086acec23c2ba3
Leonard Martin <leonard.martin@gmail.com>
2014-04-25 12:34:11 +0100

    Override module schema for specific edge cases

---
+++ schema/modules/completion_numbers.js
@@ -0,0 +1,5 @@
+var moduleSchema = require('../module');
+
+moduleSchema.definitions.axis.properties.key.required = false;
+
+module.exports = moduleSchema;
---
+++ schema/modules/grouped_timeseries.js
@@ -0,0 +1,52 @@
+var moduleSchema = require('../module');
+
+moduleSchema.definitions.axis.properties.key.required = false;
+
+module.exports = {
+  allOf: [
+    moduleSchema,
+    {
+      properties: {
+        category: {
+          type: 'string',
+          required: true
+        },
+        use_stack: {
+          type: 'boolean'
+        },
+        period: {
+          type: 'string',
+          required: true,
+          enum: [
+            'hour',
+            'day',
+            'week',
+            'month',
+            'quarter'
+          ]
+        },
+        'show-line-labels': {
+          type: 'boolean'
+        },
+        'show-total-lines': {
+          type: 'boolean'
+        },
+        'one-hundred-percent': {
+          type: 'boolean'
+        },
+        'group-mapping': {
+          type: 'object'
+        },
+        axes: {
+          type: 'object',
+          properties: {
+            y: {
+              required: true
+            }
+          }
+        }
+      }
+    }
+  ],
+  definitions: moduleSchema.definitions
+};
