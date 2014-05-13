d372be0045c6086c2beb25272f70fff18f0b9e71
Leonard Martin <leonard.martin@gmail.com>
2014-05-13 16:33:42 +0100

    Extend, rather than reimplement slightly different percentage graphs

--- app/extensions/views/timeseries_graph/percentage_graph.js
+++ app/extensions/views/timeseries_graph/percentage_graph.js
@@ -1,49 +1,22 @@
 define([
-  'common/views/visualisations/stacked-graph'
+  'common/views/visualisations/volumetrics/completion-graph'
 ],
 function (Graph) {
   var PercentageGraph = Graph.extend({
 
-    getConfigNames: function () {
-      return [this.collection.query.get('period') || 'week'];
-    },
-
-    minYDomainExtent: 1,
-    numYTicks: 3,
-
     components: function () {
-      return [
-        { view: this.sharedComponents.xaxis },
-        {
-          view: this.sharedComponents.yaxis,
-          options: {
-            tickFormat: function () {
-              return function (d) {
-                return Math.round(100 * d) + '%';
-              };
+      return Graph.prototype.components.apply(this, arguments).concat([{
+        view: this.sharedComponents.tooltip,
+        options: {
+          formatValue: function (value) {
+            value = value * 100;
+            if (value !== 100) {
+              value = value.toFixed(1);
             }
+            return value + '%';
           }
-        },
-        {
-          view: this.sharedComponents.stack,
-          options: {
-            drawCursorLine: true
-          }
-        },
-        {
-          view: this.sharedComponents.tooltip,
-          options: {
-            formatValue: function (value) {
-              value = value * 100;
-              if (value !== 100) {
-                value = value.toFixed(1);
-              }
-              return value + '%';
-            }
-          }
-        },
-        { view: this.sharedComponents.hover }
-      ];
+        }
+      }]);
     }
   });
 
