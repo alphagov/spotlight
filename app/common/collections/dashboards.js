12788bacefa1ea9d9619d4163461b86ac89fc4b4
Leonard Martin <leonard.martin@gmail.com>
2014-05-14 17:30:10 +0100

    Add filtering based on department title and abbreviation to services page

--- app/common/collections/dashboards.js
+++ app/common/collections/dashboards.js
@@ -8,7 +8,10 @@ function (Collection) {
       var groups = { count: 0 };
       this.each(function (model) {
         var title = model.get('title').toUpperCase();
-        if (!filter || title.indexOf(filter.toUpperCase()) > -1) {
+        var department = model.get('department') || { title: '', abbr: '' };
+        filter = (filter || '').toUpperCase();
+
+        if (!filter || title.indexOf(filter) > -1 || department.abbr.toUpperCase().indexOf(filter) > -1 || department.title.toUpperCase().indexOf(filter) > -1) {
           var key = title.substr(0, 1);
           groups[key] = groups[key] || [];
           groups[key].push(model.toJSON());
