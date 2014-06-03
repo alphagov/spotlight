6f5b3f7105efe0b43d658006176e1855ae251bc9
Leonard Martin <leonard.martin@gmail.com>
2014-06-03 15:19:46 +0100

    Add module controllers for client and server for individual modules to extend from

---
+++ app/client/controllers/module.js
@@ -0,0 +1,33 @@
+define([
+  'extensions/controllers/module',
+  'client/views/module'
+], function (ModuleController, ModuleView) {
+
+  return ModuleController.extend({
+
+    viewClass: ModuleView,
+
+    render: function () {
+      if (this.requiresSvg && !this.Modernizr.inlinesvg) {
+        // Do not try to render an SVG in a non-SVG browser
+        this.trigger('ready');
+        return;
+      }
+      ModuleController.prototype.render.apply(this, arguments);
+    },
+
+    viewOptions: function () {
+      var options = ModuleController.prototype.viewOptions.apply(this, arguments);
+
+      // reuse existing module slot
+      var el = $('#' + this.id());
+      if (el.length) {
+        options.el = el;
+      }
+      return options;
+    }
+
+  });
+
+
+});
---
+++ app/server/controllers/module.js
@@ -0,0 +1,11 @@
+var requirejs = require('requirejs');
+
+var ModuleController = requirejs('extensions/controllers/module');
+
+var ModuleView = require('../views/module');
+
+module.exports = ModuleController.extend({
+
+  viewClass: ModuleView
+
+});
