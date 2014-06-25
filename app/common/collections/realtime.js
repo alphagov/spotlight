041a9a85f13860c89dd108fd4102fc901569a4d2
Leonard Martin <leonard.martin@gmail.com>
2014-06-25 17:22:49 +0100

    Removed duplicate functionality from collection parse methods
    Things which can be delegated up to the global parse method now are, and only really module specific data stuff is left on the child collections.

--- app/common/collections/realtime.js
+++ app/common/collections/realtime.js
@@ -1,15 +1,15 @@
 define([
-  'common/collections/list'
+  'extensions/collections/collection'
 ],
-function (ListCollection) {
-  return ListCollection.extend({
+function (Collection) {
+  return Collection.extend({
 
     parse: function () {
-      var data = ListCollection.prototype.parse.apply(this, arguments);
+      var data = Collection.prototype.parse.apply(this, arguments);
 
       // limit the number of data point to the last <duration> <period>s
-      var latestDate = _.max(data, function (model) { return this.moment(model._timestamp); }, this);
-      var timestamp = this.moment(latestDate._timestamp);
+      var latestDate = _.max(data, function (model) { return model._timestamp; }, this);
+      var timestamp = latestDate._timestamp;
       data = _.filter(data, function (model) {
         return timestamp.diff(model._timestamp, this.options.period) < this.options.duration;
       }, this);
