9b45bbcc50978023451b76174c69b4ce2acf5e01
Anna Powell-Smith <annapowellsmith@gmail.com>
2014-06-25 14:41:43 +0100

    Fix bug on tables for touch devices.
    We should check for Modernizr touch support client-side,
    not on the server :( Update tests accordingly.
    
    I've tested this in Chrome's browser emulator and it appears
    to work.

--- spec/client/views/spec.table.js
+++ spec/client/views/spec.table.js
@@ -23,6 +23,23 @@ function (Table, $) {
         });
       });
 
+      it('adds a class of touch-table on touch devices', function () {
+        var touchTable = new Table({
+          collection: {
+            on: jasmine.createSpy(),
+            options: { axes: {
+              x: { label: 'date', key: 'timestamp' },
+              y: [{ label: 'another', key: 'value' }]
+            } }
+          },
+          valueAttr: 'value'
+        }, { touch: true });
+        var $table = $('<table></table>');
+        touchTable.$el.append($table);
+        touchTable.render();
+        expect($table.attr('class')).toContain('touch-table');
+      });
+
       it('adds a class of floated-header to the table element on the client-side when table body has more cells than the header', function () {
         var tableHeader = '<thead><tr><th>Col1</th><th>Col2</th></tr></thead>',
             tableBody = '<tbody><tr><td>Item1</td><td>Item2</td></tr><tr><td>Item1</td><td>Item2</td></tr></tbody>';
@@ -31,7 +48,7 @@ function (Table, $) {
         table.$el.append($table);
         table.render();
 
-        expect($table.attr('class')).toEqual('floated-header');
+        expect($table.attr('class')).toContain('floated-header');
       });
 
       it('doesnt a class of floated-header to the table element on the client-side when the table body has "no data"', function () {
