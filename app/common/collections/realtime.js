5bc17d0ed5359d8811b485d38cf1f0f86a0c40c5
Leonard Martin <leonard.martin@gmail.com>
2014-06-26 10:33:18 +0100

    Sort collections by timestamp by default

--- app/common/collections/realtime.js
+++ app/common/collections/realtime.js
@@ -11,12 +11,8 @@ function (Collection) {
       var latestDate = _.max(data, function (model) { return model._timestamp; }, this);
       var timestamp = latestDate._timestamp;
       data = _.filter(data, function (model) {
-        return timestamp.diff(model._timestamp, this.options.period) < this.options.duration;
+        return timestamp.diff(model._timestamp, this.options.period) <= this.options.duration;
       }, this);
-
-      data = data.sort(function (a, b) {
-        return (a._timestamp < b._timestamp) ? -1 : 1;
-      });
 
       return data;
     },
