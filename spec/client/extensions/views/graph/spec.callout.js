f96c77ef77ee71430a8707513e1261e110cb5186
Leonard Martin <leonard.martin@gmail.com>
2014-06-30 16:30:57 +0100

    Fix tests for graph callout component

--- spec/client/extensions/views/graph/spec.callout.js
+++ spec/client/extensions/views/graph/spec.callout.js
@@ -7,14 +7,11 @@ function (Callout, Model) {
 
     describe('onChangeSelected', function () {
 
-      var callout, model, group;
+      var callout, model;
       beforeEach(function () {
 
-        group = new Model({
-          title: 'test title'
-        });
-
         model = new Model({
+          title: 'test title',
           fraction: 0.3,
           a: 20,
           b: 30,
@@ -50,52 +47,58 @@ function (Callout, Model) {
         callout.render();
       });
 
-      it('hides when only a group but no item is selected', function () {
-        callout.onChangeSelected('group', 1, null, null);
+      it('hides when no model is selected', function () {
+        callout.onChangeSelected(null, null);
         expect(callout.calloutEl).toHaveClass('performance-hidden');
         expect(callout.renderContent).not.toHaveBeenCalled();
       });
 
-      it('hides when all items at a specific position are selected', function () {
-        callout.onChangeSelected(null, null, 'model', 1);
-        expect(callout.calloutEl).toHaveClass('performance-hidden');
-        expect(callout.renderContent).not.toHaveBeenCalled();
+      it('shows when a model is selected', function () {
+        callout.onChangeSelected(model, 1);
+        expect(callout.calloutEl).not.toHaveClass('performance-hidden');
+        expect(callout.renderContent).toHaveBeenCalled();
       });
 
       it('renders a callout at the correct position when graph is not scaled', function () {
         callout.graph.scaleFactor.andReturn(1);
-        callout.onChangeSelected(group, 1, model, 2);
-        expect(callout.renderContent).toHaveBeenCalledWith(
-          callout.calloutEl, group, 1, model, 2
-        );
+        callout.onChangeSelected(model, 2);
+
+        expect(callout.renderContent).toHaveBeenCalledWith(callout.calloutEl, model, undefined);
         expect(callout.calloutEl).not.toHaveClass('performance-hidden');
         expect(callout.calloutEl.css('left')).toEqual('23px'); // left margin + result of x() + offsetX
         expect(callout.calloutEl.css('top')).toEqual('43px'); // right margin + result of y() + offsetY
       });
 
+      it('passes value attribute to renderContent if a particular group is selected', function () {
+        callout.graph.scaleFactor.andReturn(1);
+        callout.onChangeSelected(model, 2, { valueAttr: 'foo' });
+
+        expect(callout.renderContent).toHaveBeenCalledWith(callout.calloutEl, model, 'foo');
+      });
+
       it('renders a callout at the correct position when graph is scaled', function () {
         callout.graph.scaleFactor.andReturn(0.5);
-        callout.onChangeSelected(group, 1, model, 2);
-        expect(callout.renderContent).toHaveBeenCalledWith(
-          callout.calloutEl, group, 1, model, 2
-        );
+        callout.onChangeSelected(model, 2);
+
+        expect(callout.renderContent).toHaveBeenCalledWith(callout.calloutEl, model, undefined);
         expect(callout.calloutEl).not.toHaveClass('performance-hidden');
         expect(callout.calloutEl.css('left')).toEqual('8px'); // (left margin + result of x()) * scaleFactor + offsetX
         expect(callout.calloutEl.css('top')).toEqual('18px'); // (right margin + result of y()) * scaleFactor + offsetY
       });
 
       it('displays series title, count', function () {
-        callout.onChangeSelected(group, 1, model, 2);
+        callout.onChangeSelected(model, 2);
         expect(callout.calloutEl.html()).toEqual('<h3>test header</h3><dl><dt>test title</dt><dd>20</dd></dl>');
       });
 
       it('displays series title, count and percentage when configured', function () {
         var testModel = new Model({
+          title: 'test title',
           a: 0.2,
           a_original: 30
         });
         callout.showPercentage = true;
-        callout.onChangeSelected(group, 1, testModel, 2);
+        callout.onChangeSelected(testModel, 2);
         expect(callout.calloutEl.html()).toEqual('<h3>test header</h3><dl><dt>test title</dt><dd>20% (30)</dd></dl>');
       });
     });
