1d886c1ab0dc21fbefc6f7f5e27564cb6e989e1b
Leonard Martin <leonard.martin@gmail.com>
2014-07-01 16:16:19 +0100

    Stripped down stacked line label tests to only test what has been extended

--- spec/client/extensions/views/graph/spec.stacked-linelabel.js
+++ spec/client/extensions/views/graph/spec.stacked-linelabel.js
@@ -1,132 +1,68 @@
 define([
   'extensions/views/graph/stacked-linelabel',
-  'extensions/collections/matrix'
+  'extensions/collections/collection'
 ],
 function (LineLabel, Collection) {
 
   describe('Stacked Line Label Component', function () {
 
-    describe('setLabelPositions', function () {
-      var el, wrapper, lineLabel, graph, collection,
-          figcaption, labelWrapper, selection, enterSelection;
+    var el, wrapper, lineLabel, collection, graph;
+    beforeEach(function () {
+
+      collection = new Collection([
+        { _count: 1 }
+      ]);
+
+      graph = {
+        innerWidth: 400,
+        valueAttr: '_count',
+        getLines: function () {
+          return [
+            { label: 'Title 1', key: '_count' }
+          ];
+        },
+        isOneHundredPercent: function () {
+          return false;
+        },
+        getYPos: function (i) { return i * 10; },
+        getY0Pos: function (i) { return i * 5 }
+      };
+
+      el = $('<div></div>').appendTo($('body'));
+      wrapper = LineLabel.prototype.d3.select(el[0]).append('svg').append('g');
+
+      lineLabel = new LineLabel({
+        collection: collection
+      });
+      lineLabel.wrapper = wrapper;
+      lineLabel.offset = 100;
+      lineLabel.linePaddingInner = 20;
+      lineLabel.linePaddingOuter = 30;
+      lineLabel.graph = graph;
+      lineLabel.margin = {
+        top: 100,
+        right: 200,
+        bottom: 300,
+        left: 400
+      };
+    });
+
+    afterEach(function () {
+      el.remove();
+    });
+
+    describe('getYIdeal', function () {
+
       beforeEach(function () {
-        collection = new Collection([
-          {
-            id: 'a',
-            values: new Collection([
-              { _count: 1, alternative: 8 },
-              { _count: 4, alternative: 5 },
-              { _count: 7, alternative: 2 }
-            ])
-          },
-          {
-            id: 'b',
-            values: new Collection([
-              { _count: 2, alternative: 9 },
-              { _count: 5, alternative: 6 },
-              { _count: 8, alternative: 3 }
-            ])
-          }
-        ]);
-
-        var yScale = jasmine.createSpy();
-        yScale.plan = function (val) {
-          return val * val;
-        };
-        graph = {
-          valueAttr: '_count',
-          innerWidth: 100,
-          innerHeight: 100,
-          getYPos: function (groupIndex, modelIndex) {
-            return collection.at(groupIndex).get('values').at(modelIndex).get(graph.valueAttr);
-          },
-          getY0Pos: function (groupIndex, modelIndex) {
-            if (groupIndex > 0) {
-              return collection.at(groupIndex - 1).get('values').at(modelIndex).get(graph.valueAttr);
-            } else {
-              return 0;
-            }
-          }
-        };
-        lineLabel = new LineLabel({
-          scales: {
-            y: yScale
-          },
-          interactive: false,
-          collection: collection,
-          margin: {
-            top: 0,
-            right: 0
-          },
-          offset: 100,
-          linePaddingInner: 20,
-          linePaddingOuter: 30,
-          graph: graph
-        });
-
-        el = $('<div></div>').appendTo($('body'));
-        wrapper = lineLabel.d3.select(el[0]);
-
-        figcaption = wrapper.selectAll('figcaption').data(['one-figcaption']);
-        figcaption.enter().append('figcaption').attr('class', 'legend');
-
-        labelWrapper = figcaption.selectAll('ol').data(['one-wrapper']);
-        labelWrapper.enter().append('ol');
-
-        // Set the height of the li for cross-browser test compatibility
-        selection = labelWrapper.selectAll('li').data(collection.models);
-        enterSelection = selection.enter().append('li').attr('style', 'display:block;height:20px;position:absolute;');
-
-        spyOn(lineLabel, 'calcPositions').andReturn([
-          { min: 20, id: 'a' },
-          { min: 30, id: 'b' }
-        ]);
+        spyOn(lineLabel.graph, 'getYPos').andCallThrough();
+        spyOn(lineLabel.graph, 'getY0Pos').andCallThrough();
       });
 
-      afterEach(function () {
-        el.remove();
-      });
-
-      it('positions labels closest to the centre of the area', function () {
-        lineLabel.setLabelPositions(wrapper.selectAll('li'));
-        expect(lineLabel.calcPositions).toHaveBeenCalled();
-        var startPositions = lineLabel.calcPositions.argsForCall[0][0];
-        expect(lineLabel.scales.y).toHaveBeenCalledWith(3.5);
-        expect(startPositions[0]).toEqual({
-          ideal: 12.25, // centre of first area
-          size: 20,
-          id: 'a'
-        });
-        expect(lineLabel.scales.y).toHaveBeenCalledWith(7.5);
-        expect(startPositions[1]).toEqual({
-          ideal: 56.25, // centre of second area
-          size: 20,
-          id: 'b'
-        });
-
-        expect($(wrapper.selectAll('li')[0][0]).prop('style').top).toEqual('20px');
-        expect($(wrapper.selectAll('li')[0][1]).prop('style').top).toEqual('30px');
-      });
-
-      it('uses the last non-null value for positioning', function () {
-        collection.at(0).get('values').last().set('_count', null);
-        collection.at(1).get('values').last().set('_count', null);
-
-        lineLabel.setLabelPositions(wrapper.selectAll('li'));
-        expect(lineLabel.calcPositions).toHaveBeenCalled();
-        var startPositions = lineLabel.calcPositions.argsForCall[0][0];
-        expect(lineLabel.scales.y).toHaveBeenCalledWith(2);
-        expect(startPositions[0]).toEqual({
-          ideal: 4, // centre of first area when using penultimate value
-          size: 20,
-          id: 'a'
-        });
-        expect(lineLabel.scales.y).toHaveBeenCalledWith(4.5);
-        expect(startPositions[1]).toEqual({
-          ideal: 20.25, // centre of second area when using penultimate value
-          size: 20,
-          id: 'b'
-        });
+      it('attempts to centre label in stack', function () {
+        var result = lineLabel.getYIdeal(1, 'foo');
+        expect(lineLabel.graph.getYPos).toHaveBeenCalledWith(1, 'foo');
+        expect(lineLabel.graph.getY0Pos).toHaveBeenCalledWith(1, 'foo');
+        expect(result).toEqual(7.5);
       });
 
     });
