2c0816199bb911de093797d7c7948b103b42587a
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-07-01 16:58:24 +0100

    Write out agencies and departments config files

--- tools/generate-services-list.js
+++ tools/generate-services-list.js
@@ -15,6 +15,9 @@ var dashboards = [
   }
 ];
 
+var departments = [],
+    agencies = [];
+
 function readModule(file) {
   var defer = Q.defer();
   fs.readFile(file, 'utf8', function (err, dashboardData) {
@@ -31,6 +34,9 @@ function readModule(file) {
     dashboardData = JSON.parse(dashboardData);
     if (dashboardData['page-type'] === 'dashboard' && dashboardData['published']) {
       dashboards.push(_.pick(dashboardData, 'slug', 'title', 'department', 'agency', 'dashboard-type', 'on-homepage'));
+
+      departments.push(_.pick(dashboardData, 'department').department);
+      agencies.push(_.pick(dashboardData, 'agency').agency);
     }
     defer.resolve();
 
@@ -48,6 +54,31 @@ glob(stagecraftStubGlob, function (err, files) {
   })).then(function () {
     console.log('Writing ' + dashboards.length + ' dashboards into dashboards.json');
     fs.writeFileSync(stagecraftStubDir + '/dashboards.json', JSON.stringify({ items: dashboards }, null, 2) + '\n');
+
+    _.each({departments: departments, agencies: agencies}, function (organisationList, organisation) {
+      organisationList = _.chain(organisationList)
+        .without(undefined)
+        .map(function (item) {
+          if (!item.abbr) {
+            item.abbr = item.title;
+          }
+          if (!item.slug) {
+            item.slug = item.abbr.toLowerCase().replace(/ /g, '-');
+          }
+          return item;
+        })
+        .sortBy(function (item) {
+          return item.title;
+        })
+        .uniq(function (item) {
+          return item.title + item.slug;
+        })
+        .value();
+
+      console.log('Writing ' + organisationList.length + ' ' + organisation + ' into ' + organisation + '.json');
+      fs.writeFileSync(stagecraftStubDir + '/' + organisation + '.json', JSON.stringify(organisationList, null, 2) + '\n');
+    });
+
   });
 
 });
