ec6133ce43592a8d9edb2f83c0848e2e16907e6a
Tom Booth <tombooth@gmail.com>
2014-07-01 16:42:37 +0100

    Add in new data source model
    This model will receive the standard data-source properties from a
    module and generate urls for use in the collection.

---
+++ app/extensions/models/data_source.js
@@ -0,0 +1,99 @@
+define([
+  'backbone',
+  'Mustache',
+  'lodash',
+  'moment-timezone'
+],
+function (Backbone, Mustache, _, moment) {
+
+  var backdropUrl;
+  if (isServer) {
+    backdropUrl = config.backdropUrl;
+  } else if (isClient) {
+    backdropUrl = GOVUK.config.backdropUrl;
+  }
+
+  var DataSource = Backbone.Model.extend({
+
+    backdropUrl: backdropUrl,
+
+    buildUrl: function (customQueryParams) {
+      var url = Mustache.render(this.backdropUrl, this.attributes),
+          rawQueryParams = _.merge(
+            {},
+            this.get('query-params') || {},
+            customQueryParams
+          ), queryParams;
+
+      if (!_.isEmpty(rawQueryParams)) {
+        url += '?' + this._objectToQueryString(
+          this.configureTimespans(rawQueryParams));
+      }
+
+      return url;
+    },
+
+    groupedBy: function () {
+      var queryParams = this.get('query-params');
+      return (queryParams && queryParams['group_by']) || null;
+    },
+
+    configureTimespans: function (queryParams) {
+      if (queryParams['start_at'] && !queryParams['end_at'] && !queryParams['duration']) {
+        queryParams['end_at'] = moment().utc().format(DataSource.ISO_8601);
+      }
+      if (queryParams.period && !queryParams.duration &&
+            !(queryParams['start_at'] && queryParams['end_at'])) {
+        queryParams.duration =
+          DataSource.PERIOD_TO_DURATION[queryParams.period];
+      }
+      return queryParams;
+    },
+
+    _encodeValue: function (value) {
+      var utcMoment;
+      if (moment.isMoment(value)) {
+        utcMoment = value.clone().utc();
+        value = value.format(DataSource.ISO_8601);
+      }
+      return encodeURIComponent(value);
+    },
+
+    _objectToQueryString: function (obj) {
+      return _.reduce(obj, function(parts, value, key) {
+        var out;
+
+        if (!(_.isNull(value) || _.isUndefined(value))) {
+          if (_.isArray(value)) {
+            out = _.map(value, function(sub_value) {
+              return this._encodeValue(key) + '=' +
+                this._encodeValue(sub_value);
+            }, this).join('&');
+          } else {
+            out = this._encodeValue(key) + '=' + this._encodeValue(value);
+          }
+
+          parts.push(out);
+        }
+
+        return parts
+      }, [], this).join('&');
+    }
+
+  }, {
+
+    ISO_8601: 'YYYY-MM-DD[T]HH:mm:ss[Z]',
+
+    PERIOD_TO_DURATION: {
+      hour: 24,
+      day: 30,
+      week: 9,
+      month: 12,
+      quarter: 24
+    }
+
+  });
+
+  return DataSource;
+
+});
