31b9b03624cd1d7c958f35ebc0f394ae18d850a2
Leonard Martin <leonard.martin@gmail.com>
2014-07-02 12:44:17 +0100

    Fix bar chart tests following removal of the matrix

--- spec/shared/common/views/visualisations/bar-chart/spec.bar-chart.js
+++ spec/shared/common/views/visualisations/bar-chart/spec.bar-chart.js
@@ -1,37 +1,37 @@
 define([
   'common/views/visualisations/bar-chart/bar-chart',
   'extensions/views/graph/graph',
-  'common/collections/journey',
   'extensions/collections/collection'
 ],
-function (BarChart, Graph, JourneyCollection, Collection) {
+function (BarChart, Graph, Collection) {
 
   describe('BarChart', function () {
     var graph;
 
     beforeEach(function () {
       graph = new BarChart({
-        collection: new JourneyCollection([{}]),
+        collection: new Collection(),
         formatOptions: { 'type': 'integer', 'magnitude': 'true' }
       });
     });
 
     describe('calcXScale', function () {
+
       it('sets the domain to the number of steps', function () {
 
-        graph.collection.at(0).set('values', new Collection([
+        graph.collection.reset([
           { uniqueEvents: 100 },
           { uniqueEvents: 100 },
           { uniqueEvents: 100 }
-        ]));
+        ]);
         expect(graph.calcXScale().domain()).toEqual([0, 2]);
 
-        graph.collection.at(0).set('values', new Collection([
+        graph.collection.reset([
           { uniqueEvents: 100 },
           { uniqueEvents: 100 },
           { uniqueEvents: 100 },
           { uniqueEvents: 100 }
-        ]));
+        ]);
         expect(graph.calcXScale().domain()).toEqual([0, 3]);
       });
     });
@@ -39,7 +39,7 @@ function (BarChart, Graph, JourneyCollection, Collection) {
     describe('getYPos', function () {
       it('returns 0 if model data is undefined', function () {
         spyOn(Graph.prototype, 'getYPos');
-        expect(graph.getYPos(0, 0)).toEqual(0);
+        expect(graph.getYPos(0)).toEqual(0);
       });
     });
 
