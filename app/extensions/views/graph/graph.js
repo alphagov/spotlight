3bbb69bd89e7b2ca617ccdf6d60d2955cd8c4d94
Leonard Martin <leonard.martin@gmail.com>
2014-07-18 10:42:33 +0100

    Store a value for the period on the graph
    The loops in `getAxisPeriod` cause a massive performance hit when called as part of a `mousemove` event, since it's called in every `modelToDate` call. By storing a value and resetting when the collection changes we can mitigate this.

--- app/extensions/views/graph/graph.js
+++ app/extensions/views/graph/graph.js
@@ -36,7 +36,10 @@ function (View, d3, XAxis, YAxis, YAxisRight, Line, Stack, Hover, Tooltip, Missi
       View.prototype.initialize.apply(this, arguments);
 
       var collection = this.collection = options.collection;
-      this.listenTo(collection, 'reset add remove sync', this.render);
+      this.listenTo(collection, 'reset add remove sync', function () {
+        this.getAxisPeriod();
+        this.render();
+      });
 
       this.prepareGraphArea();
 
@@ -114,7 +117,7 @@ function (View, d3, XAxis, YAxis, YAxisRight, Line, Stack, Hover, Tooltip, Missi
 
     modelToDate: function (model) {
       var prop = '_start_at';
-      var period = this.getAxisPeriod();
+      var period = this.axisperiod || this.getAxisPeriod();
       if (period === 'hour') {
         prop = '_timestamp';
       } else if (period === 'week' || period === 'quarter') {
