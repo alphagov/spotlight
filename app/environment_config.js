03b9c4771ac04b3c7f65bcdb03722e84c7995b7e
Leonard Martin <leonard.martin@gmail.com>
2014-08-12 13:54:22 +0100

    Remove the requirejs-ness from purely node modules
    Using requirejs instead of require for modules which are only ever executed in a Node environment is a significant code smell.

--- app/environment_config.js
+++ app/environment_config.js
@@ -1,26 +1,21 @@
-define([
-  'path',
-  'fs'
-],
-function (path, fs) {
+var path = require('path');
+var fs = require('fs');
 
-  return {
-    configure: function (env, options) {
+module.exports = {
+  configure: function (env, options) {
 
-      var configFileName = function (key) {
-        return ['config.', key, '.json'].join('');
-      };
+    var configFileName = function (key) {
+      return ['config.', key, '.json'].join('');
+    };
 
-      // If we're in development and the personal gitignored config file exists, use it
-      if (env === 'development' && fs.existsSync(path.join('config', configFileName('development_personal')))) {
-        env = 'development_personal';
-      }
-
-      var config = JSON.parse(fs.readFileSync(path.join('config', configFileName(env))));
-
-      var filteredOptions = _.pick(options, _.keys(config));
-      return _.extend(config, filteredOptions);
+    // If we're in development and the personal gitignored config file exists, use it
+    if (env === 'development' && fs.existsSync(path.join('config', configFileName('development_personal')))) {
+      env = 'development_personal';
     }
-  };
 
-});
+    var config = JSON.parse(fs.readFileSync(path.join('config', configFileName(env))));
+
+    var filteredOptions = _.pick(options, _.keys(config));
+    return _.extend(config, filteredOptions);
+  }
+};
