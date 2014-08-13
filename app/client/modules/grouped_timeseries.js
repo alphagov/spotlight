53e66b497fdd2f9e4104e9091b22eacce4a9fcfb
Leonard Martin <leonard.martin@gmail.com>
2014-08-13 11:34:45 +0100

    Move grouped-timeseries visualisation views to client directory

--- app/client/modules/grouped_timeseries.js
+++ app/client/modules/grouped_timeseries.js
@@ -1,7 +1,7 @@
 define([
   'client/controllers/module',
   'common/modules/grouped_timeseries',
-  'common/views/visualisations/grouped_timeseries'
+  'client/views/visualisations/grouped_timeseries'
 ], function (ModuleController, GroupedTimeseriesModule, GroupedTimeseriesView) {
 
   return ModuleController.extend(GroupedTimeseriesModule).extend({
