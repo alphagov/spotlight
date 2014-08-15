9cbeefa07ee3b8779eac1efcf1ee8c42e7b93dd0
Leonard Martin <leonard.martin@gmail.com>
2014-08-15 11:28:54 +0100

    Fix model lookup for page-type
    Was looking for model.page-type, which broke image fallbacks on page-per-thing. The actual value was on the parent.

--- app/extensions/controllers/dashboard.js
+++ app/extensions/controllers/dashboard.js
@@ -10,7 +10,7 @@ define([
         this.model,
         function (model) {
           return {
-            url: model.get('page-type') === 'module' ? this.url : this.url + '/' + model.get('slug')
+            url: model.get('parent').get('page-type') === 'module' ? this.url : this.url + '/' + model.get('slug')
           };
         }.bind(this),
         { init: options.init },
