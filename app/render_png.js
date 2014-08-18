914d061879732bb1857078ef35d3e3db145728b8
Leonard Martin <leonard.martin@gmail.com>
2014-08-18 11:26:09 +0100

    Add "nice" error pages for SSaaS failures
    SSaaS now returns errors with appropriate status codes when requesting a page to screenshot fails. We can use these to render nice errors in spotlight when following broken PNG links.

--- app/render_png.js
+++ app/render_png.js
@@ -1,18 +1,40 @@
 var http = require('http');
 var url = require('url');
 
+var requirejs = require('requirejs');
+
+var ErrorController = require('./server/controllers/error');
+var Model = requirejs('extensions/models/model');
+var PageConfig = requirejs('page_config');
+
 var renderPng = function (req, res) {
   var options = url.parse(renderPng.getScreenshotPath(req));
   http.get(options, function (screenshot) {
-    res.status(screenshot.statusCode);
-    for (var i in screenshot.headers) {
-      res.setHeader(i, screenshot.headers[i]);
+    if (screenshot.statusCode > 399) {
+      return renderError(screenshot.statusCode, req, res);
+    } else {
+      res.status(screenshot.statusCode);
+      for (var i in screenshot.headers) {
+        res.setHeader(i, screenshot.headers[i]);
+      }
+      screenshot.pipe(res);
     }
-    screenshot.pipe(res);
-  }).on('error', function (e) {
-    res.status(500);
-    res.send('Got error: ' + e.message);
+  }).on('error', function () {
+    renderError(500, req, res);
   });
+};
+
+var renderError = function (status, req, res) {
+  var model = new Model({
+    status: status || 500
+  });
+  model.set(PageConfig.commonConfig(req));
+  var error = new ErrorController({
+    model: model
+  });
+  error.render();
+  res.statusCode = status;
+  res.send(error.html);
 };
 
 if (global.config) {
