53e245240522b20d1025399df7cd75488b4fe333
Jacob Ashdown <jcbashdown@gmail.com>
2014-08-29 16:30:31 +0100

    Tests for fetch with fallback
    also refactor tests - some common things into context, some not common
    moved out of common context

--- spec/shared/spec.stagecraft_api_client.js
+++ spec/shared/spec.stagecraft_api_client.js
@@ -1,7 +1,8 @@
 define([
-  'stagecraft_api_client'
+  'stagecraft_api_client',
+  'extensions/models/model'
 ],
-function (StagecraftApiClient) {
+function (StagecraftApiClient, Model) {
   describe('StagecraftApiClient', function () {
 
     var ControllerMap = {
@@ -12,31 +13,63 @@ function (StagecraftApiClient) {
       }
     };
 
-    beforeEach(function () {
-      spyOn(StagecraftApiClient.prototype, 'fetch');
-
+    describe('fetch', function (){
+      beforeEach(function () {
+        spyOn(Model.prototype, 'fetch');
+      });
+      it('fetches from stagecraft before falling back to local files', function () {
+        var client = new StagecraftApiClient({
+          path: 'foo',
+          ControllerMap: ControllerMap
+        });
+        expect(Model.prototype.fetch.callCount).toEqual(0);
+        client.fetch();
+        expect(Model.prototype.fetch.callCount).toEqual(2);
+      });
     });
 
-    it('retrieves data for the current path', function () {
-      var client = new StagecraftApiClient({}, {
-        ControllerMap: ControllerMap
+    describe('testing client.url', function() {
+      var client;
+      beforeEach(function () {
+        spyOn(StagecraftApiClient.prototype, 'fetch');
+        client = new StagecraftApiClient({}, {
+          ControllerMap: ControllerMap
+        });
+        client.stagecraftUrlRoot = 'http://boosh/';
+        client.urlRoot = 'http://testdomain/';
       });
-      client.urlRoot = 'http://testdomain/';
-      client.setPath('foo/bar');
-      expect(client.url()).toEqual('http://testdomain/foo/bar');
+      describe('when fallback is false', function () {
+        it('should use the stagecraftUrlRoot', function () {
+          client.fallback = false 
+          client.setPath('foo/bar');
+          expect(client.url()).toEqual('http://boosh/foo/bar');
+        });
+      });
+      describe('when fallback is true', function () {
+        it('should use the urlRoot', function () {
+          client.fallback = true 
+          client.setPath('foo/bar');
+          expect(client.url()).toEqual('http://testdomain/foo/bar');
+        });
+      });
     });
 
-    it('re-retrieves data when the path changes', function () {
-      var client = new StagecraftApiClient({
-        path: 'foo'
-      }, {
-        ControllerMap: ControllerMap
+    describe('setPath', function (){
+      beforeEach(function (){
+        spyOn(StagecraftApiClient.prototype, 'fetch');
       });
-      expect(client.fetch.callCount).toEqual(0);
-      client.setPath('foo/bar');
-      expect(client.fetch.callCount).toEqual(1);
-      client.setPath('foo/bar');
-      expect(client.fetch.callCount).toEqual(2);
+      it('re-retrieves data when the path changes', function () {
+        var client = new StagecraftApiClient({
+          path: 'foo'
+        }, {
+          ControllerMap: ControllerMap
+        });
+        expect(client.fetch.callCount).toEqual(0);
+        client.setPath('foo/bar');
+        expect(client.fetch.callCount).toEqual(1);
+        client.setPath('foo/bar');
+        expect(client.fetch.callCount).toEqual(2);
+      });
     });
 
     describe('parse', function () {
