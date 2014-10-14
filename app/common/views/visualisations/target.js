5ff9688c1f03bf988ef8d892c6b2c8b50c4861c9
Ben Marvell <madmarv187@gmail.com>
2014-10-14 18:29:15 +0100

    Target view
    Calculates a percentage target based on the modules this.target
    
    Has a dirty hack because we don't flattern the data out of backdrop properly yet.
    
    We shouldn't be newing it up through a model like this the data should just be flat.
    
    Needs more pretty.

---
+++ app/common/views/visualisations/target.js
@@ -0,0 +1,51 @@
+define([
+  'common/views/visualisations/volumetrics/number',
+  'extensions/models/model'
+],
+function (SingleStatView, Model) {
+  var TargetNumberView = SingleStatView.extend({
+
+    getValue: function () {
+      var target = this.target;
+      var total = 0;
+      var greaterThanTarget = 0;
+      var targetPercent = 1;
+
+      this.collection.each(function (model) {
+        total += model.get(this.valueAttr);
+        if (target <= model.get(this.pinTo)) {
+          greaterThanTarget += model.get(this.valueAttr);
+        }
+      }, this);
+
+      targetPercent = targetPercent - (greaterThanTarget / total);
+
+      return this.format(targetPercent, 'percent');
+    },
+
+    render: function () {
+      var content;
+      content = '<' + this.valueTag + '>' + this.getValue() + '</' + this.valueTag + '>';
+      content += '<span>' + this.getLabel() + '</span>';
+      content += '<span>' + this.getDateRange() + '</span>';
+      this.$el.html(content);
+    },
+
+    getLabel: function () {
+      return 'processed within ' + this.target + ' working days';
+    },
+
+    getDateRange: function () {
+      var presentMonth = _.last(this.collection.first().get('values'));
+
+      return this.formatPeriod(new Model(presentMonth), this.collection.getPeriod());
+    },
+
+    getPeriod: function () {
+      return this.model.get('axis-period') || 'week';
+    }
+  });
+
+  return TargetNumberView;
+
+});
