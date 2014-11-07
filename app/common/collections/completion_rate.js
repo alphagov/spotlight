845cc5e8e02e6716b79f5103087462dd5e21ca91
Tim Mower <timothy.mower@gmail.com>
2014-11-07 15:27:19 +0000

    Change completion rate to work with flat data
     - re implement based on flattened data structure
     - do this and explicily match on start date rather than relying on
       order we unflatten data in - concerned this would not match the order
       it comes from backdrop
     - branches for flat and unflat data remain - needs clearing up when we
       are happy with flat data working

--- app/common/collections/completion_rate.js
+++ app/common/collections/completion_rate.js
@@ -9,36 +9,65 @@ define([
     },
 
     parse: function (response) {
-      var values = [];
-      if (response.data && response.data.length > 0 && response.data[0].values) {
+      var values = [],
+      data = response.data;
+      if (data && data.length > 0 && (data[0].values || this.flat)) {
         values = this.calculateCompletion(response.data);
       }
       return values;
     },
 
     calculateCompletion: function (dataset) {
-      return _.map(dataset[0].values, function (model, i) {
-        var totals = _.reduce(dataset, function (memo, d) {
-          if (d.values[i][this.valueAttr] !== null) {
-            if (d[this.matchingAttribute].match(this.denominatorMatcher) !== null) {
-              memo.start += d.values[i][this.valueAttr];
-            }
-            if (d[this.matchingAttribute].match(this.numeratorMatcher) !== null) {
-              memo.end += d.values[i][this.valueAttr];
-            }
+      if (this.flat) {
+        return _.map(_.filter(dataset, function(data) {
+          return data[this.matchingAttribute] && data[this.matchingAttribute].match(this.denominatorMatcher);
+        }, this), function (model) {
+          var matchingStart, matchingEnd;
+          if (model[this.valueAttr] !== null) {
+            matchingStart = _.find(dataset, function (data) {
+              return data['_start_at'] === model['_start_at'] && data[this.matchingAttribute] && data[this.matchingAttribute].match(this.denominatorMatcher);
+            }, this);
+            matchingEnd = _.find(dataset, function (data) {
+              return data['_end_at'] === model['_end_at'] && data[this.matchingAttribute] && data[this.matchingAttribute].match(this.numeratorMatcher);
+            }, this);
           }
-          return memo;
-        }, {start: null, end: null}, this);
+          var totals = {};
+          totals['start'] = (matchingStart ? matchingStart[this.valueAttr]: null);
+          totals['end'] = (matchingEnd ? matchingEnd[this.valueAttr]: null);
 
-        var value = {
-          _start_at: this.getMoment(model._start_at),
-          _end_at: this.getMoment(model._end_at),
-          _start: totals.start,
-          _end: totals.end
-        };
-        return _.extend(this.defaultValueAttrs(value), value);
+          var value = {
+            _start_at: this.getMoment(model._start_at),
+            _end_at: this.getMoment(model._end_at),
+            _start: totals.start,
+            _end: totals.end
+          };
+          return _.extend(this.defaultValueAttrs(value), value);
 
-      }, this);
+        }, this);
+      } else {
+        return _.map(dataset[0].values, function (model, i){
+          var totals = _.reduce(dataset, function (memo, d) {
+            if (d.values[i][this.valueAttr] !== null) {
+              if (d[this.matchingAttribute].match(this.denominatorMatcher) !== null) {
+                memo.start += d.values[i][this.valueAttr];
+              }
+              if (d[this.matchingAttribute].match(this.numeratorMatcher) !== null) {
+                memo.end += d.values[i][this.valueAttr];
+              }
+            }
+            return memo;
+          }, {start: null, end: null}, this);
+
+          var value = {
+            _start_at: this.getMoment(model._start_at),
+            _end_at: this.getMoment(model._end_at),
+            _start: totals.start,
+            _end: totals.end
+          };
+          return _.extend(this.defaultValueAttrs(value), value);
+
+        }, this);
+      }
     },
 
     mean: function (attr) {
