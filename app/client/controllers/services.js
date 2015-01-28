188544e5689064b2bc7438ae226d46414b8cd2d5
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-01-28 14:50:29 +0000

    Services page - use table instead of filtered list. Add summary figure view

--- app/client/controllers/services.js
+++ app/client/controllers/services.js
@@ -2,12 +2,29 @@ define([
   'extensions/controllers/controller',
   'common/collections/dashboards',
   'client/views/services'
-], function (Controller, FilteredListCollection, ServicesView) {
+], function (Controller, DashboardsCollection, ServicesView) {
   return Controller.extend({
-    collectionClass: FilteredListCollection,
+    collectionClass: DashboardsCollection,
     viewClass: ServicesView,
     clientRenderOnInit: true,
+
+    initialize: function() {
+      this.listenTo(this.model, 'change:filter change:departmentFilter', this.updateCollectionFilter);
+    },
+
+    updateCollectionFilter: function () {
+      var filteredList = this.unfilteredCollection.filterServices({
+        text: this.model.get('filter'),
+        department: this.model.get('departmentFilter')
+      });
+
+      this.collection.reset(filteredList);
+    },
+
     renderView: function (options) {
+      this.unfilteredCollection = new DashboardsCollection(this.collection.models, this.collection.options);
+
+      this.updateCollectionFilter();
       options = _.extend({}, this.viewOptions(), options);
       if (!this.view) {
         this.view = new this.viewClass(options);
@@ -18,6 +35,9 @@ define([
     },
     collectionData: function () {
       return this.model.get('data');
+    },
+    collectionOptions: function() {
+      return this.model.get('axesOptions');
     },
     viewOptions: function () {
       return {
--- spec/shared/common/collections/spec.dashboards.js
+++ spec/shared/common/collections/spec.dashboards.js
@@ -1,207 +1,186 @@
 define([
-  'common/collections/dashboards'
-],
-function (Collection) {
-  describe('Filtered List Collection', function () {
+    'common/collections/dashboards'
+  ],
+  function (Collection) {
+    describe('Filtered List Collection', function () {
 
-    var collection;
-    var data = [
-      { title: 'Sheep' },
-      { title: 'Cow' },
-      { title: 'Pig' },
-      { title: 'Chicken' },
-      { title: 'Duck' }
-    ];
-
-    beforeEach(function () {
-      collection = new Collection(data);
-    });
-
-    it('sorts by title', function () {
-      expect(collection.at(0).get('title')).toEqual('Chicken');
-      expect(collection.at(1).get('title')).toEqual('Cow');
-      expect(collection.at(2).get('title')).toEqual('Duck');
-      expect(collection.at(3).get('title')).toEqual('Pig');
-      expect(collection.at(4).get('title')).toEqual('Sheep');
-    });
-
-    describe('alphabetise', function () {
-
-      it('groups by first letter', function () {
-
-        var output = collection.alphabetise();
-
-        expect(_.keys(output)).toEqual(['count', 'C', 'D', 'P', 'S']);
-        expect(output.C).toEqual([
-          { title: 'Chicken' },
-          { title: 'Cow' }
-        ]);
-        expect(output.D).toEqual([
-          { title: 'Duck' }
-        ]);
-        expect(output.P).toEqual([
-          { title: 'Pig' }
-        ]);
-        expect(output.S).toEqual([
-          { title: 'Sheep' }
-        ]);
-
-      });
-
-      it('keeps a count of total entries', function () {
-        var output = collection.alphabetise();
-        expect(output.count).toEqual(5);
-      });
-
-      it('filters input based on string matching of a single character', function () {
-        var output = collection.alphabetise({'text': 'C'});
-        expect(output.count).toEqual(3);
-        expect(output.C).toEqual([
-          { title: 'Chicken' },
-          { title: 'Cow' }
-        ]);
-        expect(output.D).toEqual([
-          { title: 'Duck' }
-        ]);
-        expect(output.P).toBeUndefined();
-        expect(output.S).toBeUndefined();
-      });
-
-      it('filters input based on string matching of multiple characters', function () {
-        var output = collection.alphabetise({'text': 'ck'});
-        expect(output.count).toEqual(2);
-        expect(output.C).toEqual([
-          { title: 'Chicken' },
-        ]);
-        expect(output.D).toEqual([
-          { title: 'Duck' }
-        ]);
-        expect(output.P).toBeUndefined();
-        expect(output.S).toBeUndefined();
-      });
-
-      it('filters input based on string matching against department title', function () {
-
-        collection.reset([
-          { title: 'Foo', department: { title: 'Department of Things', abbr: 'DoT' } },
-          { title: 'Bar', department: { title: 'Department of Other Stuff', abbr: 'DoOS' } }
-        ]);
-
-        var output = collection.alphabetise({'text': 'thing'});
-        expect(output.count).toEqual(1);
-        expect(output.F).toEqual([
-          { title: 'Foo', department: { title: 'Department of Things', abbr: 'DoT' } }
-        ]);
-
-      });
-
-      it('filters input based on string matching against department abbreviation', function () {
-
-        collection.reset([
-          { title: 'Foo', department: { title: 'Department of Things', abbr: 'DoT' } },
-          { title: 'Bar', department: { title: 'Department of Other Stuff', abbr: 'DoOS' } }
-        ]);
-
-        var output = collection.alphabetise({'text': 'doos'});
-        expect(output.count).toEqual(1);
-        expect(output.B).toEqual([
-          { title: 'Bar', department: { title: 'Department of Other Stuff', abbr: 'DoOS' } }
-        ]);
-
-      });
-
-      it('filters based on explicit department filter', function () {
-        collection.reset([
-          { title: 'Blood', department: { title: 'Department of Health', abbr: 'DH' } },
-          { title: 'Passport', department: { title: 'Home Office', abbr: 'Home Office' } }
-        ]);
-
-        var output = collection.alphabetise({'department': 'home-office'});
-        expect(output.count).toEqual(1);
-        expect(output.P).toEqual([
-          { title: 'Passport', department: { title: 'Home Office', abbr: 'Home Office' } }
-        ]);
-      });
-
-    });
-
-    describe('getSlug', function () {
-      it('lowercases the abbreviation if possible', function () {
-        var department = { title: 'Cabinet Office', abbr: 'CO' };
-        expect(collection.getSlug(department)).toEqual('co');
-      });
-
-      it('turns spaces into hyphens', function () {
-        var department = { title: 'Home Office', abbr: 'Home Office' };
-        expect(collection.getSlug(department)).toEqual('home-office');
-      });
-
-      it('uses the title if no abbreviation is provided', function () {
-        var department = { title: 'Skills Funding Agency' };
-        expect(collection.getSlug(department)).toEqual('skills-funding-agency');
-      });
-
-      it('returns unknown-organisation if no useful information is provided', function () {
-        var department = { foo: 'bar' };
-        expect(collection.getSlug(department)).toEqual('unknown-organisation');
-      });
-    });
-
-    describe('filterDashboards', function () {
-
+      var collection;
       var data = [
-        { title: 'Sheep', 'dashboard-type': 'transaction' },
-        { title: 'Cow', 'dashboard-type': 'high-volume-transaction' },
-        { title: 'Pig', 'dashboard-type': 'service-group' },
-        { title: 'Chicken', 'dashboard-type': 'transaction' },
-        { title: 'Duck', 'dashboard-type': 'transaction' }
+        {title: 'Sheep'},
+        {title: 'Cow'},
+        {title: 'Pig'},
+        {title: 'Chicken'},
+        {title: 'Duck'}
       ];
 
       beforeEach(function () {
-        collection.reset(data);
+        collection = new Collection(data);
       });
 
-      it('filters the collection to only the dashboard types provided', function () {
-        var output;
-        output = collection.filterDashboards('service-group');
-        expect(output).toEqual([
-          { title: 'Pig', 'dashboard-type': 'service-group' }
-        ]);
-
-        output = collection.filterDashboards('transaction');
-        expect(output).toEqual([
-          { title: 'Chicken', 'dashboard-type': 'transaction' },
-          { title: 'Duck', 'dashboard-type': 'transaction' },
-          { title: 'Sheep', 'dashboard-type': 'transaction' }
-        ]);
+      it('sorts by title', function () {
+        expect(collection.at(0).get('title')).toEqual('Chicken');
+        expect(collection.at(1).get('title')).toEqual('Cow');
+        expect(collection.at(2).get('title')).toEqual('Duck');
+        expect(collection.at(3).get('title')).toEqual('Pig');
+        expect(collection.at(4).get('title')).toEqual('Sheep');
       });
 
-      it('handles multiple values', function () {
-        var output;
-        output = collection.filterDashboards('service-group');
-        expect(output).toEqual([
-          { title: 'Pig', 'dashboard-type': 'service-group' }
-        ]);
+      describe('getSlug', function () {
+        it('lowercases the abbreviation if possible', function () {
+          var department = {title: 'Cabinet Office', abbr: 'CO'};
+          expect(collection.getSlug(department)).toEqual('co');
+        });
 
-        output = collection.filterDashboards('transaction', 'service-group');
-        expect(output).toEqual([
-          { title: 'Chicken', 'dashboard-type': 'transaction' },
-          { title: 'Duck', 'dashboard-type': 'transaction' },
-          { title: 'Pig', 'dashboard-type': 'service-group' },
-          { title: 'Sheep', 'dashboard-type': 'transaction' }
-        ]);
+        it('turns spaces into hyphens', function () {
+          var department = {title: 'Home Office', abbr: 'Home Office'};
+          expect(collection.getSlug(department)).toEqual('home-office');
+        });
+
+        it('uses the title if no abbreviation is provided', function () {
+          var department = {title: 'Skills Funding Agency'};
+          expect(collection.getSlug(department)).toEqual('skills-funding-agency');
+        });
+
+        it('returns unknown-organisation if no useful information is provided', function () {
+          var department = {foo: 'bar'};
+          expect(collection.getSlug(department)).toEqual('unknown-organisation');
+        });
       });
 
-      it('handles an array as the first argument', function () {
-        var output;
-        output = collection.filterDashboards(['service-group']);
-        expect(output).toEqual([
-          { title: 'Pig', 'dashboard-type': 'service-group' }
-        ]);
+      describe('filterServices', function () {
+        var data = [
+          {
+            title: 'Prescriptions: prepayment certificates issued',
+            'department': {
+              'title': 'Department of Health',
+              'abbr': 'DH'
+            },
+            agency: {
+              title: 'NHS Business Services Authority',
+              abbr: 'NHSBSA'
+            }
+          },
+          {
+            'title': 'Written GP referrals to first outpatient appointment',
+            'department': {
+              'title': 'Department of Health',
+              'abbr': 'DH'
+            },
+            'agency': {
+              'title': 'NHS England',
+              'abbr': 'NHS England'
+            }
+          },
+          {
+            'title': 'Job search adviser interventions',
+            'department': {
+              'title': 'Department for Work and Pensions',
+              'abbr': 'DWP'
+            }
+          }
+        ];
+
+        beforeEach(function () {
+          collection.reset(data);
+        });
+
+        it('filters the collection using a combination of search string and dept', function () {
+          var filter = {
+              text: 'GP',
+              department: 'dh'
+            },
+            models = collection.filterServices(filter);
+          expect(models[0].get('title')).toEqual('Written GP referrals to first outpatient appointment');
+        });
+
+        it('filters on search string only', function () {
+          var filter = {
+              text: 'health'
+            },
+            models;
+          models = collection.filterServices(filter);
+          expect(models.length).toEqual(2);
+          expect(models[0].get('title')).toEqual('Prescriptions: prepayment certificates issued');
+          expect(models[1].get('title')).toEqual('Written GP referrals to first outpatient appointment');
+        });
+
+        it('filters on agency only', function () {
+          var filter = {
+              department: 'agency:nhsbsa'
+            },
+            models;
+          models = collection.filterServices(filter);
+          expect(models.length).toEqual(1);
+          expect(models[0].get('title')).toEqual('Prescriptions: prepayment certificates issued');
+        });
+
+        it('filters on department only', function () {
+          var filter = {
+              department: 'dwp'
+            },
+            models;
+          models = collection.filterServices(filter);
+          expect(models.length).toEqual(1);
+          expect(models[0].get('title')).toEqual('Job search adviser interventions');
+        });
+
       });
+
+      describe('filterDashboards', function () {
+
+        var data = [
+          {title: 'Sheep', 'dashboard-type': 'transaction'},
+          {title: 'Cow', 'dashboard-type': 'high-volume-transaction'},
+          {title: 'Pig', 'dashboard-type': 'service-group'},
+          {title: 'Chicken', 'dashboard-type': 'transaction'},
+          {title: 'Duck', 'dashboard-type': 'transaction'}
+        ];
+
+        beforeEach(function () {
+          collection.reset(data);
+        });
+
+        it('filters the collection to only the dashboard types provided', function () {
+          var output;
+          output = collection.filterDashboards('service-group');
+          expect(output).toEqual([
+            {title: 'Pig', 'dashboard-type': 'service-group'}
+          ]);
+
+          output = collection.filterDashboards('transaction');
+          expect(output).toEqual([
+            {title: 'Chicken', 'dashboard-type': 'transaction'},
+            {title: 'Duck', 'dashboard-type': 'transaction'},
+            {title: 'Sheep', 'dashboard-type': 'transaction'}
+          ]);
+        });
+
+        it('handles multiple values', function () {
+          var output;
+          output = collection.filterDashboards('service-group');
+          expect(output).toEqual([
+            {title: 'Pig', 'dashboard-type': 'service-group'}
+          ]);
+
+          output = collection.filterDashboards('transaction', 'service-group');
+          expect(output).toEqual([
+            {title: 'Chicken', 'dashboard-type': 'transaction'},
+            {title: 'Duck', 'dashboard-type': 'transaction'},
+            {title: 'Pig', 'dashboard-type': 'service-group'},
+            {title: 'Sheep', 'dashboard-type': 'transaction'}
+          ]);
+        });
+
+        it('handles an array as the first argument', function () {
+          var output;
+          output = collection.filterDashboards(['service-group']);
+          expect(output).toEqual([
+            {title: 'Pig', 'dashboard-type': 'service-group'}
+          ]);
+        });
+
+      });
+
 
     });
-
-
   });
-});
