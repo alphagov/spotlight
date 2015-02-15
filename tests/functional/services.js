3f22cfc44727eab5008b784fbdcd31ec90da4b79
Ben Marvell <madmarv187@gmail.com>
2015-02-15 21:06:49 +0000

    Refactored tests to stop repetition.

--- tests/functional/services.js
+++ tests/functional/services.js
@@ -1,4 +1,9 @@
 module.exports = {
+  selectors: {
+    'filterTable': '#filtered-list table',
+    'textFilter': '#filter',
+    'kpi': '.service-kpis'
+  },
 
   beforeEach: function (client) {
     client
@@ -16,74 +21,74 @@ module.exports = {
 
   'Text filter': function (client) {
     client
-      .assert.containsText('#filtered-list table', 'Vehicle tax renewals')
-      .setValue('#filter', 'PAYE')
-      .waitForElementVisible('#filtered-list table', 1000)
-      .assert.doesNotContainText('#filtered-list table', 'Vehicle tax renewals')
-      .assert.containsText('#filtered-list table', 'PAYE transactions')
+      .assert.containsText(this.selectors.filterTable, 'Vehicle tax renewals')
+      .setValue(this.selectors.textFilter, 'PAYE')
+      .waitForElementVisible(this.selectors.filterTable, 1000)
+      .assert.doesNotContainText(this.selectors.filterTable, 'Vehicle tax renewals')
+      .assert.containsText(this.selectors.filterTable, 'PAYE transactions')
       .end();
   },
 
   'Dropdown filter': function (client) {
     client
-      .assert.containsText('#filtered-list table', 'Vehicle tax renewals')
+      .assert.containsText(this.selectors.filterTable, 'Vehicle tax renewals')
       .click('#department option[value="co"]')
-      .waitForElementVisible('#filtered-list table', 1000)
-      .assert.doesNotContainText('#filtered-list table', 'Vehicle tax renewals')
-      .assert.containsText('#filtered-list table', 'e-petition signatures')
+      .waitForElementVisible(this.selectors.filterTable, 1000)
+      .assert.doesNotContainText(this.selectors.filterTable, 'Vehicle tax renewals')
+      .assert.containsText(this.selectors.filterTable, 'e-petition signatures')
       .end();
   },
 
   'Text and Dropdown filter': function (client) {
     client
-      .assert.containsText('#filtered-list table', 'Vehicle tax renewals')
+      .assert.containsText(this.selectors.filterTable, 'Vehicle tax renewals')
       .click('#department option[value="co"]')
-      .waitForElementVisible('#filtered-list table', 1000)
-      .assert.containsText('#filtered-list table', 'Digital Services Store')
-      .assert.containsText('#filtered-list table', 'Digital Marketplace')
-      .setValue('#filter', 'Digital Marketplace')
-      .waitForElementVisible('#filtered-list table', 1000)
-      .assert.doesNotContainText('#filtered-list table', 'Digital Services Store')
-      .assert.containsText('#filtered-list table', 'Digital Marketplace')
+      .waitForElementVisible(this.selectors.filterTable, 1000)
+      .assert.containsText(this.selectors.filterTable, 'Digital Services Store')
+      .assert.containsText(this.selectors.filterTable, 'Digital Marketplace')
+      .setValue(this.selectors.textFilter, 'Digital Marketplace')
+      .waitForElementVisible(this.selectors.filterTable, 1000)
+      .assert.doesNotContainText(this.selectors.filterTable, 'Digital Services Store')
+      .assert.containsText(this.selectors.filterTable, 'Digital Marketplace')
       .end();
   },
 
   'KPI Values': function (client) {
     client
-      .assert.containsText('.service-kpis .number_of_transactions', 'Transactions per year')
-      .assert.doesNotContainText('.service-kpis .number_of_transactions', 'no data')
-      .assert.containsText('.service-kpis .total_cost', 'Annual cost')
-      .assert.doesNotContainText('.service-kpis .total_cost', 'no data')
-      .assert.containsText('.service-kpis .cost_per_transaction', 'Cost per transaction')
-      .assert.doesNotContainText('.service-kpis .cost_per_transaction', 'no data')
-      .assert.containsText('.service-kpis .user_satisfaction_score', 'User satisfaction')
-      .assert.doesNotContainText('.service-kpis .user_satisfaction_score', 'no data')
-      .assert.containsText('.service-kpis .completion_rate', 'Completion rate')
-      .assert.doesNotContainText('.service-kpis .completion_rate', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .number_of_transactions', 'Transactions per year')
+      .assert.doesNotContainText(this.selectors.kpi + ' .number_of_transactions', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .total_cost', 'Annual cost')
+      .assert.doesNotContainText(this.selectors.kpi + ' .total_cost', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .cost_per_transaction', 'Cost per transaction')
+      .assert.doesNotContainText(this.selectors.kpi + ' .cost_per_transaction', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .user_satisfaction_score', 'User satisfaction')
+      .assert.doesNotContainText(this.selectors.kpi + ' .user_satisfaction_score', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .completion_rate', 'Completion rate')
+      .assert.doesNotContainText(this.selectors.kpi + ' .completion_rate', 'no data')
       .end();
   },
 
   'no data': function (client) {
     client
-      .setValue('#filter', 'no data can be found')
-      .assert.containsText('.service-kpis .number_of_transactions', 'Transactions per year')
-      .assert.containsText('.service-kpis .number_of_transactions', 'no data')
-      .assert.containsText('.service-kpis .total_cost', 'Annual cost')
-      .assert.containsText('.service-kpis .total_cost', 'no data')
-      .assert.containsText('.service-kpis .cost_per_transaction', 'Cost per transaction')
-      .assert.containsText('.service-kpis .cost_per_transaction', 'no data')
-      .assert.containsText('.service-kpis .user_satisfaction_score', 'User satisfaction')
-      .assert.containsText('.service-kpis .user_satisfaction_score', 'no data')
-      .assert.containsText('.service-kpis .completion_rate', 'Completion rate')
-      .assert.containsText('.service-kpis .completion_rate', 'no data')
+      .setValue(this.selectors.textFilter, 'no data can be found')
+      .assert.containsText(this.selectors.kpi + ' .number_of_transactions', 'Transactions per year')
+      .assert.containsText(this.selectors.kpi + ' .number_of_transactions', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .total_cost', 'Annual cost')
+      .assert.containsText(this.selectors.kpi + ' .total_cost', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .cost_per_transaction', 'Cost per transaction')
+      .assert.containsText(this.selectors.kpi + ' .cost_per_transaction', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .user_satisfaction_score', 'User satisfaction')
+      .assert.containsText(this.selectors.kpi + ' .user_satisfaction_score', 'no data')
+      .assert.containsText(this.selectors.kpi + ' .completion_rate', 'Completion rate')
+      .assert.containsText(this.selectors.kpi + ' .completion_rate', 'no data')
       .end();
   },
 
   'Clicking on a kpi summary link': function (client) {
     client
-      .assert.cssClassPresent('#filtered-list th[data-key="number_of_transactions"]', 'descending')
-      .click('.service-kpis .number_of_transactions .visualisation-moreinfo a')
-      .assert.cssClassPresent('#filtered-list th[data-key="number_of_transactions"]', 'ascending')
+      .assert.cssClassPresent(this.selectors.filterTable + ' th[data-key="number_of_transactions"]', 'descending')
+      .click(this.selectors.kpi + ' .number_of_transactions .visualisation-moreinfo a')
+      .assert.cssClassPresent(this.selectors.filterTable + ' th[data-key="number_of_transactions"]', 'ascending')
       .end();
   }
 
