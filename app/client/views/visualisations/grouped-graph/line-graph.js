9c94c88974f86a08f5efad63ef263665cbf45c95
Jon Wyatt <jonnywyatt@yahoo.co.uk>
2015-03-10 16:21:14 +0000

    For grouped line graph, set min Y value to zero
    So that modules with many low values have a sensible Y axis range.
    eg. f4ee691b553804da4f95d8c2d6328d60/de-qos/de-packet-loss

--- app/client/views/visualisations/grouped-graph/line-graph.js
+++ app/client/views/visualisations/grouped-graph/line-graph.js
@@ -8,6 +8,8 @@ function (Graph, LineSet) {
 
     GroupClass: LineSet,
 
+    minYDomainExtent: 0,
+
     maxValue: function () {
       return _.reduce(this.getLines(), function (max, line) {
         return Math.max(max, this.collection.max(line.key) || 0);
