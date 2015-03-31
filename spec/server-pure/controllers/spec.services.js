8e701e34a65ed6215f631d1f8269cf9bf83c4db9
James Abley <james.abley@gmail.com>
2015-03-31 10:27:40 +0100

    Specific caching directives for error HTML
    If an attempt to render a dashboard fails, we should not cache the
    response for the normal period. Instead, allow a small window to allow
    the system to recover if it’s under load.

--- spec/server-pure/controllers/spec.services.js
+++ spec/server-pure/controllers/spec.services.js
@@ -203,4 +203,13 @@ describe('Services Controller', function () {
     expect(res.set).toHaveBeenCalledWith('Cache-Control', 'public, max-age=7200');
   });
 
+  it('has a shorter explicit caching policy for errors', function () {
+    client_instance = controller('services', req, res);
+    client_instance.set({
+      'status': 500
+    });
+    client_instance.trigger('sync');
+    expect(res.set).toHaveBeenCalledWith('Cache-Control', 'public, max-age=5');
+  });
+
 });
