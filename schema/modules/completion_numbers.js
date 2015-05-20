6b9bdc9bb507fc0d1feca623da5f11dcd091464f
Ben Marvell <madmarv187@gmail.com>
2015-05-20 16:08:46 +0100

    Remove schema from spotlight.
    The schema for modules is now happening at the stagecraft layer.
    
    This schema is out of date and no longer required

--- schema/modules/completion_numbers.js
+++
@@ -1,8 +0,0 @@
-var moduleSchema = require('../module');
-var _ = require('lodash');
-
-moduleSchema = _.cloneDeep(moduleSchema);
-
-moduleSchema.definitions.axis.properties.key.required = false;
-
-module.exports = moduleSchema;
