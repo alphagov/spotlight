189e2283aa06fe3902cc870fedc0fdd2f0e9c4a6
Caroline Green <carolinegreen@gds3696.local>
2015-09-16 15:01:09 +0100

    Fix link to json summary not updating when dates are changed.
    Update link to json summary in the client view based on selection in date picker.  Keeping
    old json link based on server side as a fallback for when javascript isn't enabled.

---
+++ spec/client/views/spec.json-summary.js
@@ -0,0 +1,36 @@
+define([
+  'client/views/json-summary',
+  'extensions/models/model',
+  'extensions/collections/collection',
+  'jquery'
+],
+function (JsonSummary, Model, Collection, $) {
+  describe('JsonSummary', function () {
+    var view, $el, model, collection;
+
+    collection =  new Collection();
+
+    beforeEach(function () {
+      $el = $('<li class="json-summary"></li>');
+      $el.html('<a href="<jsonUrl>">JSON</a>');
+      view = new JsonSummary({
+        el: $el,
+        model: model,
+        collection: collection
+      });
+    });
+
+    describe('render', function () {
+      it('should update element to add new url assigned', function () {
+        spyOn(collection, 'url').andReturn('newUrl');
+        var $oldEl = view.$el;
+        view.render();
+        expect($oldEl).not.toEqual(view.$el);
+        expect(view.$el.attr('href')).toContain('newUrl');
+      });
+
+    });
+
+  });
+});
+
