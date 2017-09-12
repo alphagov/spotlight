475eb3eea1c8a5d5f966433bb12a77d2cc48146f
Paul Craig <paul_craig_16@hotmail.com>
2017-09-12 11:30:25 +0100

    More verbose cheapseats tests
    The dot reporter wasn't really giving back a lot of information
    about what was going wrong with some of the test failures.
    
    source: https://mochajs.org/#dot-matrix

--- Gruntfile.js
+++ Gruntfile.js
@@ -261,7 +261,7 @@ module.exports = function (grunt) {
           stderr: true,
           stdout: true
         },
-        command: addArgs('node ./node_modules/cheapseats/index.js --reporter dot-retry --standalone --path --quickrun ./')
+        command: addArgs('node ./node_modules/cheapseats/index.js --reporter spec-retry --standalone --path --quickrun ./')
       },
       // Runs all cheapseats tests
       cheapseats_full_run: {
@@ -270,7 +270,7 @@ module.exports = function (grunt) {
           stderr: true,
           stdout: true
         },
-        command: addArgs('node ./node_modules/cheapseats/index.js --reporter dot-retry --standalone --path ./')
+        command: addArgs('node ./node_modules/cheapseats/index.js --reporter spec-retry --standalone --path ./')
       },
       nightwatch: {
         command: addArgs('./node_modules/nightwatch/bin/nightwatch')
