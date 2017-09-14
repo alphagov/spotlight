1386409d7f000a27e50d6af4cc6382e684269a09
Paul Craig <paul_craig_16@hotmail.com>
2017-09-14 13:23:23 +0100

    Whitespace changes only

--- app/client/views/json-summary.js
+++ app/client/views/json-summary.js
@@ -5,23 +5,23 @@ define([
 
     initialize: function () {
 
-        this.listenTo(this.collection, 'reset add remove', this.render);
+      this.listenTo(this.collection, 'reset add remove', this.render);
     },
 
-    render: function(){
+    render: function() {
 
-        var url = this.collection.url();
-        var urlJson = url+'&format=json';
-        var urlCSV = url + '&format=csv';
-        var $newEl;
-        var $oldEl = this.$el;
+      var url = this.collection.url();
+      var urlJson = url+'&format=json';
+      var urlCSV = url + '&format=csv';
+      var $newEl;
+      var $oldEl = this.$el;
 
-        $newEl = $('<a href="' + urlJson + '">JSON</a> | <a href="' + urlCSV + '">CSV</a>');
+      $newEl = $('<a href="' + urlJson + '">JSON</a> | <a href="' + urlCSV + '">CSV</a>');
 
-        this.setElement($newEl);
-        $oldEl.replaceWith($newEl);
+      this.setElement($newEl);
+      $oldEl.replaceWith($newEl);
 
-        return this;
+      return this;
     }
 
  });
