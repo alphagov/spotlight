8316b9da94ea78c0e26d8bc8003eee600f370380
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-12-11 12:44:30 +0000

    drop blue-green in favour of cf push
    blue-green doesn't support newer manifest fields and is taking control
    of mapping routes (which we want to control seperately), if we want
    zero-downtime deploys again in the future we can use the v3 apis

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -15,10 +15,7 @@ wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key |
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 sudo apt-get update && sudo apt-get install cf-cli
 
-# Login and install the BGD plugin for the zero downtime deploy
 cf login -u $PAAS_USER -p $PAAS_PASSWORD -a https://api.cloud.service.gov.uk -o gds-performance-platform -s $PAAS_SPACE
-cf install-plugin -f blue-green-deploy -r CF-Community
 
-# Use BGD to push the app to NAME-new and then it will be renamed to NAME once successful
-cf blue-green-deploy performance-platform-spotlight-$PAAS_SPACE -f manifest.$PAAS_SPACE.yml
+cf push -f manifest.$PAAS_SPACE.yml
 
